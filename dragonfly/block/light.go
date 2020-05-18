206b4eaf6649aeb9664736cf570fea2ba1018acb
Sandertv <st.ten.veldhuis@gmail.com>
2020-05-18 09:57:05 +0200

    block/light.go: Fixed wrong data type for light level.

--- dragonfly/block/light.go
+++ dragonfly/block/light.go
@@ -6,7 +6,12 @@ import "git.jetbrains.space/dragonfly/dragonfly.git/dragonfly/world"
 type Light struct {
 	// Level is the light level that the light block produces. It is a number from 0-15, where 15 is the
 	// brightest and 0 is no light at all.
-	Level uint8
+	Level int
+}
+
+// ReplaceableBy ...
+func (l Light) ReplaceableBy(b world.Block) bool {
+	return true
 }
 
 // EncodeItem ...
@@ -16,7 +21,7 @@ func (l Light) EncodeItem() (id int32, meta int16) {
 
 // LightEmissionLevel ...
 func (l Light) LightEmissionLevel() uint8 {
-	return l.Level
+	return uint8(l.Level)
 }
 
 // LightDiffusionLevel ...
@@ -26,13 +31,13 @@ func (l Light) LightDiffusionLevel() uint8 {
 
 // EncodeBlock ...
 func (l Light) EncodeBlock() (name string, properties map[string]interface{}) {
-	return "minecraft:light_block", map[string]interface{}{"block_light_level": l.Level}
+	return "minecraft:light_block", map[string]interface{}{"block_light_level": int32(l.Level)}
 }
 
 // allLight returns all possible light blocks.
 func allLight() []world.Block {
 	m := make([]world.Block, 0, 16)
-	for i := uint8(0); i < 16; i++ {
+	for i := 0; i < 16; i++ {
 		m = append(m, Light{Level: i})
 	}
 	return m
