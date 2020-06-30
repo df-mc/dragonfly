5d27b9bea74938bcc58c769e72407f923d63f727
Robert Duda <robertduda20@gmail.com>
2020-06-30 21:45:04 +0200

    seeding math/rand package.

--- examples/server_example.go
+++ examples/server_example.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/df-mc/dragonfly/dragonfly"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	server := dragonfly.New(nil, nil)
 	server.CloseOnProgramEnd()
 	if err := server.Start(); err != nil {
