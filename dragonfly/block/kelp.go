04302cf8d019cb768fadef365a69837c5d2a343e
DenielWorld <denielmirzoyan@gmail.com>
2020-07-14 13:02:19 -0700

    kelp.go - Requested changes (clean-ups)

--- dragonfly/block/kelp.go
+++ dragonfly/block/kelp.go
@@ -37,28 +37,25 @@ func (k Kelp) EncodeBlock() (name string, properties map[string]interface{}) {
 	return "minecraft:kelp", map[string]interface{}{"age": int32(k.Age)}
 }
 
-// CanDisplace ...
+// CanDisplace will return true if the liquid is Water, since kelp can waterlog.
 func (Kelp) CanDisplace(b world.Liquid) bool {
 	_, water := b.(Water)
 	return water
-	// Kelp can waterlog.
 }
 
-// SideClosed ...
+// SideClosed will always return false since kelp doesn't close any side.
 func (Kelp) SideClosed(pos, side world.BlockPos, w *world.World) bool {
 	return false
-	// Kelp can always be flowed through.
 }
 
-// AABB ...
+// AABB will always return nil, since Kelp can be placed even if someone is standing on its placement position.
 func (Kelp) AABB(world.BlockPos, *world.World) []physics.AABB {
 	return nil
-	// Kelp can be placed even if someone is standing on its placement position.
 }
 
-// SetRandomAge ...
-func (k Kelp) setRandomAge() Kelp {
-	// In Java Edition, the age value can be up to 25, but MCPE limits it to 15.
+// withRandomAge returns a new Kelp block with its age value randomized between 0 and 14.
+func (k Kelp) withRandomAge() Kelp {
+	// In Java Edition, Kelp's age value can be up to 25, but MCPE limits it to 15.
 	k.Age = rand.Intn(14)
 	return k
 }
@@ -76,15 +73,15 @@ func (k Kelp) UseOnBlock(pos world.BlockPos, face world.Face, _ mgl64.Vec3, w *w
 		return false
 	}
 
-	liquid, liquidExists := w.Liquid(pos)
-	// A check for existent water at this position, kelp cannot be placed if none is found.
-	if !liquidExists || liquid.LiquidType() != "water" || liquid.LiquidDepth() < 8 {
-		// Water must be a source block to plant kelp.
+	liquid, ok := w.Liquid(pos)
+	if !ok {
+		return false
+	} else if _, ok := liquid.(Water); !ok || liquid.LiquidDepth() < 8 {
 		return false
 	}
 
 	// When first placed, kelp gets a random age between 0 and 14 in MCBE.
-	place(w, pos, k.setRandomAge(), user, ctx)
+	place(w, pos, k.withRandomAge(), user, ctx)
 	return placed(ctx)
 }
 
@@ -92,7 +89,7 @@ func (k Kelp) UseOnBlock(pos world.BlockPos, face world.Face, _ mgl64.Vec3, w *w
 func (k Kelp) NeighbourUpdateTick(pos, changed world.BlockPos, w *world.World) {
 	if changed.Y()-1 == pos.Y() {
 		// When a kelp block is broken above, the kelp block underneath it gets a new random age.
-		w.PlaceBlock(pos, k.setRandomAge())
+		w.PlaceBlock(pos, k.withRandomAge())
 	}
 
 	switch w.Block(pos.Add(world.BlockPos{0, -1})).(type) {
@@ -118,16 +115,18 @@ func (k Kelp) RandomTick(pos world.BlockPos, w *world.World, r *rand.Rand) {
 		// Every random tick, there's a 14% chance for Kelp to grow if its age is below 15.
 		abovePos := pos.Add(world.BlockPos{0, 1})
 
-		liquid, liquidAboveExists := w.Liquid(abovePos)
+		liquid, ok := w.Liquid(abovePos)
 
 		// For kelp to grow, there must be only water above.
-		if liquidAboveExists && liquid.LiquidType() == "water" {
+		if !ok {
+			return
+		} else if _, ok := liquid.(Water); ok {
 			switch w.Block(abovePos).(type) {
 			case Air, Water:
 				w.PlaceBlock(abovePos, Kelp{Age: k.Age + 1})
 				if liquid.LiquidDepth() < 8 {
 					// When kelp grows into a water block, the water block becomes a source block.
-					w.SetLiquid(abovePos, Water{true, 8, false})
+					w.SetLiquid(abovePos, Water{Still: true, Depth: 8, Falling: false})
 				}
 			}
 		}
@@ -137,7 +136,7 @@ func (k Kelp) RandomTick(pos world.BlockPos, w *world.World, r *rand.Rand) {
 
 // allKelp returns all possible states of a kelp block.
 func allKelp() (b []world.Block) {
-	for i := 15; i >= 0; i-- {
+	for i := 0; i < 16; i++ {
 		b = append(b, Kelp{Age: i})
 	}
 	return
