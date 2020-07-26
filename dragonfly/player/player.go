529ca98b530e2a0b6a27f3899614905ecf40ee48
Twisted <twistedasylummc@gmail.com>
2020-07-26 11:39:46 +0100

    Control Server-Side XBOX Live Authentication (#36)
    * Add the ability to control server authentication
    
    * Change documentation & set Server.AuthenticationEnabled to default true
    
    * Fix Server.AuthEnabled value

--- dragonfly/player/player.go
+++ dragonfly/player/player.go
@@ -147,7 +147,8 @@ func (p *Player) UUID() uuid.UUID {
 // and will not change in the lifetime of an account.
 // The XUID is a number that can be parsed as an int64. No more information on what it represents is
 // available, and the UUID should be preferred.
-// The XUID returned is empty if the Player is not connected to a network session.
+// The XUID returned is empty if the Player is not connected to a network session or if the Player is not
+// authenticated with XBOX Live.
 func (p *Player) XUID() string {
 	return p.xuid
 }
