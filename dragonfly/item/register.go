70dbf8a67f6fa07b517142e3aea65271adc1b77f
DenielWorld <denielmirzoyan@gmail.com>
2020-07-26 02:00:39 -0700

    Beacon fed ores
    Adds the following:
    * Netherite Ingot
    * Gold Ingot
    * Iron Ingot
    * Emerald

--- dragonfly/item/register.go
+++ dragonfly/item/register.go
@@ -70,4 +70,8 @@ func init() {
 	world.RegisterItem("minecraft:bucket", Bucket{Content: bucket.Lava()})
 
 	world.RegisterItem("minecraft:diamond", Diamond{})
+	world.RegisterItem("minecraft:emerald", Emerald{})
+	world.RegisterItem("minecraft:gold_ingot", GoldIngot{})
+	world.RegisterItem("minecraft:iron_ingot", IronIngot{})
+	world.RegisterItem("minecraft:netherite_ingot", NetheriteIngot{})
 }
