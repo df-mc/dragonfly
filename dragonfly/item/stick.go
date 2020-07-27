c6190cb1a9ddf4e3ec4d8d6d3b7d61d87ed2cec1
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2020-07-27 10:03:35 -0500

    Added ...

--- dragonfly/item/stick.go
+++ dragonfly/item/stick.go
@@ -3,6 +3,7 @@ package item
 // Sticks are one of the most abundant resources used for crafting many tools and items.
 type Stick struct {}
 
+// EncodeItem ...
 func (s Stick) EncodeItem() (id int32, meta int16) {
 	return 280, 0
 }
