df22e00d1e38d69de5f6be76fa82222cac26d72f
ImDaRealAni <51390305+ImDaRealAni@users.noreply.github.com>
2020-07-27 11:13:26 -0500

    Added sticks (#49)
    * Added sticks
    
    * Added ...
    
    * Changed Sticks to Stick
    
    Co-authored-by: anirudh246 <51390305+anirudh246@users.noreply.github.com>

---
+++ dragonfly/item/stick.go
@@ -0,0 +1,9 @@
+package item
+
+// Stick is one of the most abundant resources used for crafting many tools and items.
+type Stick struct {}
+
+// EncodeItem ...
+func (s Stick) EncodeItem() (id int32, meta int16) {
+	return 280, 0
+}
