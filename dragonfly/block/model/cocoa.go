4f2945b49d97d35ee87a94d9158eef0a20add057
DaPigGuy <mcpepig123@gmail.com>
2020-07-28 22:57:06 -0700

    Fixed cocoa model AABB

--- dragonfly/block/model/cocoa.go
+++ dragonfly/block/model/cocoa.go
@@ -15,9 +15,11 @@ type Cocoa struct {
 // AABB ...
 func (c Cocoa) AABB(pos world.BlockPos, w *world.World) []physics.AABB {
 	return []physics.AABB{physics.NewAABB(mgl64.Vec3{}, mgl64.Vec3{1, 1, 1}).
-		Stretch(int(c.Facing.Rotate90().Face().Axis()), float64(c.Age-6)/16).
-		ExtendTowards(int(world.FaceDown), float64(c.Age-6)/16).ExtendTowards(int(world.FaceUp), -0.25).
-		ExtendTowards(int(c.Facing.Opposite()), -0.0625).ExtendTowards(int(c.Facing), float64(c.Age*2-11)/16)}
+		Stretch(int(c.Facing.Rotate90().Face().Axis()), -((6-float64(c.Age))/16)).
+		ExtendTowards(int(world.FaceDown), -((7-float64(c.Age)*2)/16)).
+		ExtendTowards(int(world.FaceUp), -0.25).
+		ExtendTowards(int(c.Facing.Face()), -0.0625).
+		ExtendTowards(int(c.Facing.Opposite().Face()), -((11 - float64(c.Age)*2) / 16))}
 }
 
 // FaceSolid ...
