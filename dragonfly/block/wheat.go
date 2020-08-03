0ef49170f93f7d5291da758967a175dc5bd8b171
DaPigGuy <mcpepig123@gmail.com>
2020-08-02 21:18:53 -0700

    Added missing docs

--- dragonfly/block/wheat.go
+++ dragonfly/block/wheat.go
@@ -8,21 +8,25 @@ import (
 	"math/rand"
 )
 
+// Wheat is a crop that can be harvested to craft bread, cake, & cookies.
 type Wheat struct {
 	noNBT
 	transparent
 	empty
 
+	// Growth is the current stage of growth. Max is 7.
 	Growth int
 }
 
+// NeighbourUpdateTick ...
 func (s Wheat) NeighbourUpdateTick(pos, _ world.BlockPos, w *world.World) {
 	if _, ok := w.Block(pos.Side(world.FaceDown)).(Farmland); !ok {
 		w.BreakBlock(pos)
 	}
 }
 
-func (s Wheat) UseOnBlock(pos world.BlockPos, face world.Face, clickPos mgl64.Vec3, w *world.World, user item.User, ctx *item.UseContext) bool {
+// UseOnBlock ...
+func (s Wheat) UseOnBlock(pos world.BlockPos, face world.Face, _ mgl64.Vec3, w *world.World, user item.User, ctx *item.UseContext) bool {
 	pos, face, used := firstReplaceable(w, pos, face, s)
 	if !used {
 		return false
@@ -36,6 +40,7 @@ func (s Wheat) UseOnBlock(pos world.BlockPos, face world.Face, clickPos mgl64.Ve
 	return placed(ctx)
 }
 
+// BreakInfo ...
 func (s Wheat) BreakInfo() BreakInfo {
 	return BreakInfo{
 		Hardness:    0,
@@ -50,14 +55,17 @@ func (s Wheat) BreakInfo() BreakInfo {
 	}
 }
 
+// EncodeItem ...
 func (s Wheat) EncodeItem() (id int32, meta int16) {
 	return 295, 0
 }
 
+// HasLiquidDrops...
 func (s Wheat) HasLiquidDrops() bool {
 	return true
 }
 
+// RandomTick ...
 func (s Wheat) RandomTick(pos world.BlockPos, w *world.World, r *rand.Rand) {
 	if s.Growth < 7 && r.Intn(2) == 0 {
 		s.Growth++
@@ -65,18 +73,22 @@ func (s Wheat) RandomTick(pos world.BlockPos, w *world.World, r *rand.Rand) {
 	}
 }
 
+// GrowthStage ...
 func (s Wheat) GrowthStage() int {
 	return s.Growth
 }
 
+// EncodeBlock ...
 func (s Wheat) EncodeBlock() (name string, properties map[string]interface{}) {
 	return "minecraft:wheat", map[string]interface{}{"growth": int32(s.Growth)}
 }
 
+// Hash ...
 func (s Wheat) Hash() uint64 {
 	return hashWheat | (uint64(s.Growth) << 32)
 }
 
+// allWheat ...
 func allWheat() (wheat []world.Block) {
 	for i := 0; i <= 7; i++ {
 		wheat = append(wheat, Wheat{Growth: i})
