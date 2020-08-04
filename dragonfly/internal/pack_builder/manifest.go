4c9e775e39d867958c0fa48206dd09a1466e4ceb
TwistedAsylumMC <twistedasylummc@gmail.com>
2020-08-04 10:47:57 +0100

    Provide a UUID based off of the resource pack hash

--- dragonfly/internal/pack_builder/manifest.go
+++ dragonfly/internal/pack_builder/manifest.go
@@ -8,13 +8,13 @@ import (
 	"path/filepath"
 )
 
-func buildManifest(dir string) {
+func buildManifest(dir string, headerUUID uuid.UUID) {
 	m, err := json.Marshal(resource.Manifest{
 		FormatVersion: 2,
 		Header: resource.Header{
 			Name:               "dragonfly auto-generated resource pack",
 			Description:        "This resource pack contains auto-generated content from dragonfly",
-			UUID:               uuid.New().String(),
+			UUID:               headerUUID.String(),
 			Version:            [3]int{0, 0, 1},
 			MinimumGameVersion: [3]int{1, 16, 0},
 		},
--- dragonfly/internal/pack_builder/resource_pack.go
+++ dragonfly/internal/pack_builder/resource_pack.go
@@ -2,6 +2,7 @@ package pack_builder
 
 import (
 	"github.com/sandertv/gophertunnel/minecraft/resource"
+	"github.com/sger/go-hashdir"
 	"io/ioutil"
 	"os"
 )
@@ -21,8 +22,14 @@ func BuildResourcePack() *resource.Pack {
 	lang = append(lang, itemLang...)
 
 	if assets > 0 {
-		buildManifest(dir)
 		buildLanguageFile(dir, lang)
+		hash, err := hashdir.Create(dir, "md5")
+		if err != nil {
+			panic(err)
+		}
+		var data [16]byte
+		copy(data[:], hash)
+		buildManifest(dir, data)
 		return resource.MustCompile(dir)
 	}
 	return nil
