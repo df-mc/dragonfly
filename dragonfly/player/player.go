e5aba682a1a136a0bcf105ba291c21da40d033df
Sandertv <st.ten.veldhuis@gmail.com>
2020-08-05 10:48:29 +0200

    player/player.go: Release food on every second UseItem call.

--- dragonfly/player/player.go
+++ dragonfly/player/player.go
@@ -860,11 +860,11 @@ func (p *Player) UseItem() {
 			if !usable.AlwaysConsumable() && (p.GameMode() != gamemode.Creative{}) && p.Food() >= 20 {
 				// The item.Consumable is not always consumable, the player is not in creative mode and the
 				// food bar is filled: The item cannot be consumed.
-				p.usingItem.Store(false)
-				p.updateState()
+				p.ReleaseItem()
 				return
 			}
 			if !p.usingItem.CAS(false, true) {
+				p.ReleaseItem()
 				// The player is currently using the item held. This is a signal the item was consumed, so we
 				// consume it and start using it again.
 				// Due to the network overhead and latency, the duration might sometimes be a little off. We
@@ -879,6 +879,7 @@ func (p *Player) UseItem() {
 				p.SetHeldItems(p.subtractItem(held, 1), left)
 				p.addNewItem(&item.UseContext{NewItem: usable.Consume(p.World(), p)})
 				p.World().PlaySound(p.Position().Add(mgl64.Vec3{0, 1.5}), sound.Burp{})
+				return
 			}
 			p.usingSince.Store(time.Now().UnixNano())
 			p.updateState()
@@ -913,9 +914,6 @@ func (p *Player) UseItemOnBlock(pos world.BlockPos, face world.Face, clickPos mg
 	p.handler().HandleItemUseOnBlock(ctx, pos, face, clickPos)
 
 	ctx.Continue(func() {
-		p.usingItem.Store(false)
-		p.updateState()
-
 		if activatable, ok := p.World().Block(pos).(block.Activatable); ok {
 			// If a player is sneaking, it will not activate the block clicked, unless it is not holding any
 			// items, in which the block will activated as usual.
