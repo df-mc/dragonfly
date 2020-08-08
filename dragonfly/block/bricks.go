8c650a84f03cdb0b5f24f17ffe2a5e43820e0e23
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2020-08-08 11:44:39 -0500

    fix hopefully

---
+++ dragonfly/block/bricks.go
@@ -0,0 +1,36 @@
+package block
+
+import (
+	"github.com/df-mc/dragonfly/dragonfly/item"
+)
+
+// Bricks are decorative building blocks.
+type Bricks struct {
+	noNBT
+	solid
+}
+
+// BreakInfo ...
+func (b Bricks) BreakInfo() BreakInfo {
+	return BreakInfo{
+		Hardness: 2,
+		Harvestable: pickaxeHarvestable,
+		Effective: pickaxeEffective,
+		Drops: simpleDrops(item.NewStack(b, 1)),
+	}
+}
+
+// EncodeItem ...
+func (Bricks) EncodeItem() (id int32, meta int16){
+	return 45, 0
+}
+
+// EncodeBlock ...
+func (b Bricks) EncodeBlock() (name string, properties map[string]interface{}) {
+	return "minecraft:brick_block", nil
+}
+
+// Hash ...
+func (b Bricks) Hash() uint64 {
+	return hashBricks
+}
