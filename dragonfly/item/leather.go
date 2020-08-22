dd76f4514d14fb26e1814c81c8e73595a4635911
Liato <64108277+Liato-GR@users.noreply.github.com>
2020-08-22 18:32:41 +0200

    Created Leather File
    Currently adds only the Item, later will be added Functionality

---
+++ dragonfly/item/leather.go
@@ -0,0 +1,9 @@
+package item
+
+// Leather is Dropped by Cows! Need to add Mobs too but still Leather is now here
+type Leather struct{}
+
+// EncodeItem ...
+func (Leather) EncodeItem() (id int32, meta int16) {
+	return 334, 0
+}
