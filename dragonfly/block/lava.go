c2e2f20476d0162c94dec92f0e94a93dfdef2a7d
DaPigGuy <mcpepig123@gmail.com>
2020-08-29 03:11:15 -0700

    Fixed missing LavaFlammable check

--- dragonfly/block/lava.go
+++ dragonfly/block/lava.go
@@ -42,7 +42,7 @@ func (l Lava) RandomTick(pos world.BlockPos, w *world.World, r *rand.Rand) {
 		for j := 0; j < i; j++ {
 			pos = pos.Add(world.BlockPos{r.Intn(3) - 1, 1, r.Intn(3) - 1})
 			if _, ok := w.Block(pos).(Air); ok {
-				if neighboursFlammable(pos, w) {
+				if flammable, ok := w.Block(pos).(Flammable); ok && flammable.FlammabilityInfo().LavaFlammable {
 					w.PlaceBlock(pos, Fire{})
 				}
 			}
