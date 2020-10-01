2d3a88153a39f7059366245a5cf63c942a8e6341
Unickorn <nick@npbe.net>
2020-10-01 16:42:34 +0300

    Implemented Invisible Bedrock

---
+++ dragonfly/block/invisible_bedrock.go
@@ -0,0 +1,24 @@
+package block
+
+// InvisibleBedrock is an indestructible, solid block, similar to bedrock and has the appearance of air.
+// It shares many of its properties with barriers.
+type InvisibleBedrock struct {
+	noNBT
+	transparent
+	solid
+}
+
+// EncodeItem ...
+func (InvisibleBedrock) EncodeItem() (id int32, meta int16) {
+	return 95, 0
+}
+
+// EncodeBlock ...
+func (InvisibleBedrock) EncodeBlock() (name string, properties map[string]interface{}) {
+	return "minecraft:invisibleBedrock", nil
+}
+
+// Hash ...
+func (InvisibleBedrock) Hash() uint64 {
+	return hashInvisibleBedrock
+}
--- dragonfly/block/register.go
+++ dragonfly/block/register.go
@@ -106,6 +106,7 @@ func init() {
 	world.RegisterBlock(BlueIce{})
 	world.RegisterBlock(GildedBlackstone{})
 	world.RegisterBlock(Shroomlight{})
+	world.RegisterBlock(InvisibleBedrock{})
 }
 
 func init() {
@@ -288,6 +289,7 @@ func init() {
 	world.RegisterItem("minecraft:blue_ice", BlueIce{})
 	world.RegisterItem("minecraft:gilded_blackstone", GildedBlackstone{})
 	world.RegisterItem("minecraft:shroomlight", Shroomlight{})
+	world.RegisterItem("minecraft:invisibleBedrock", InvisibleBedrock{})
 }
 
 func init() {
