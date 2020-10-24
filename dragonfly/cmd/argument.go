725edd542d8ec563ea26a319a6988dcc39e7e6ee
Sandertv <st.ten.veldhuis@gmail.com>
2020-10-24 19:29:56 +0200

    cmd/argument.go: Use strings.EqualFold.

--- dragonfly/cmd/argument.go
+++ dragonfly/cmd/argument.go
@@ -170,7 +170,7 @@ func (p parser) enum(line *Line, val reflect.Value, v Enum) error {
 	}
 	found := ""
 	for _, option := range v.Options() {
-		if strings.ToLower(option) == strings.ToLower(arg) {
+		if strings.EqualFold(option, arg) {
 			found = option
 		}
 	}
