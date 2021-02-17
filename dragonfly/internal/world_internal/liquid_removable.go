edf3fc67baeae679b21b0a03f4ad1150d75ab2b2
Tal <justtal@protonmail.com>
2021-02-17 05:31:26 -0600

    Revert liquid removable updates to fix water issues (#104)
    * Revert liquid removable updates to fix water issues
    
    * Update air runtime ID
    
    * Switch back to an array and set value to true instead of HasLiquidDrops
    
    * woops
    
    * more woops
    
    * even more retarded
    
    i hate GitHub web lmao
    
    * Formatting fixes

--- dragonfly/internal/world_internal/liquid_removable.go
+++ dragonfly/internal/world_internal/liquid_removable.go
@@ -1,6 +1,6 @@
 package world_internal
 
-// LiquidRemovable is a map indexed by the runtime IDs of blocks. If present in this map, a block with that
+// LiquidRemovable is an array indexed by the runtime IDs of blocks. If present in this array, a block with that
 // runtime ID is removable by liquid and either will or will not drop items, depending on the bool in the map,
 // when flown into by a liquid.
 var LiquidRemovable []bool
