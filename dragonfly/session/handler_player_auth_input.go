a16f9b17aee192ed665c4b002bb8774a4c5bbf2e
Sandertv <st.ten.veldhuis@gmail.com>
2021-04-30 16:13:01 +0200

    session/handler_player_auth_input.go: Reduce distance for teleport back-and-forth prevention hack.

--- dragonfly/session/handler_player_auth_input.go
+++ dragonfly/session/handler_player_auth_input.go
@@ -37,7 +37,7 @@ func (h PlayerAuthInputHandler) handleMovement(pk *packet.PlayerAuthInput, s *Se
 	teleportPos := s.teleportPos
 	s.teleportMu.Unlock()
 	if teleportPos != nil {
-		if newPos.Sub(*teleportPos).Len() > 2 {
+		if newPos.Sub(*teleportPos).Len() > 0.5 {
 			// The player has moved before it received the teleport packet. Ignore this movement entirely and
 			// wait for the client to sync itself back to the server. Once we get a movement that is close
 			// enough to the teleport position, we'll allow the player to move around again.
