8ab70ae1b1ac6c1fcaf11df74be7529ef6a4465c
DaPigGuy <mcpepig123@gmail.com>
2021-05-01 19:43:06 -0700

    Fixed inconsistencies with vanilla Minecraft

--- server/block/barrier.go
+++ server/block/barrier.go
@@ -1,9 +1,25 @@
 package block
 
+import (
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/world"
+)
+
 // Barrier is a transparent solid block used to create invisible boundaries.
 type Barrier struct {
 	transparent
 	solid
+}
+
+// CanDisplace ...
+func (Barrier) CanDisplace(b world.Liquid) bool {
+	_, water := b.(Water)
+	return water
+}
+
+// SideClosed ...
+func (Barrier) SideClosed(cube.Pos, cube.Pos, *world.World) bool {
+	return false
 }
 
 // EncodeItem ...
--- server/block/farmland.go
+++ server/block/farmland.go
@@ -79,6 +79,11 @@ func (f Farmland) EncodeBlock() (name string, properties map[string]interface{})
 	return "minecraft:farmland", map[string]interface{}{"moisturized_amount": int32(f.Hydration)}
 }
 
+// EncodeItem ...
+func (f Farmland) EncodeItem() (id int32, name string, meta int16) {
+	return 60, "minecraft:farmland", 0
+}
+
 // allFarmland returns all possible states that a block of farmland can be in.
 func allFarmland() (b []world.Block) {
 	for i := 0; i <= 7; i++ {
