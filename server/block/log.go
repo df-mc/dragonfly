cd44fbabc3c599a6459623a90a6b44d918d82173
DaPigGuy <mcpepig123@gmail.com>
2021-06-04 00:49:35 -0700

    block/log.go: Implemented nether log variants

--- server/block/log.go
+++ server/block/log.go
@@ -25,6 +25,9 @@ type Log struct {
 
 // FlammabilityInfo ...
 func (l Log) FlammabilityInfo() FlammabilityInfo {
+	if !l.Wood.Flammable() {
+		return newFlammabilityInfo(0, 0, false)
+	}
 	return newFlammabilityInfo(5, 5, true)
 }
 
@@ -52,37 +55,21 @@ func (l Log) Strip() (world.Block, bool) {
 
 // EncodeItem ...
 func (l Log) EncodeItem() (name string, meta int16) {
+	if !l.Stripped {
+		switch l.Wood {
+		case OakWood(), SpruceWood(), BirchWood(), JungleWood():
+			return "minecraft:log", int16(l.Wood.Uint8())
+		case AcaciaWood(), DarkOakWood():
+			return "minecraft:log2", int16(l.Wood.Uint8()) - 4
+		case CrimsonWood(), WarpedWood():
+			return "minecraft:" + l.Wood.String() + "_stem", 0
+		}
+	}
 	switch l.Wood {
-	case OakWood():
-		if l.Stripped {
-			return "minecraft:stripped_oak_log", 0
-		}
-		return "minecraft:log", 0
-	case SpruceWood():
-		if l.Stripped {
-			return "minecraft:stripped_spruce_log", 0
-		}
-		return "minecraft:log", 1
-	case BirchWood():
-		if l.Stripped {
-			return "minecraft:stripped_birch_log", 0
-		}
-		return "minecraft:log", 2
-	case JungleWood():
-		if l.Stripped {
-			return "minecraft:stripped_jungle_log", 0
-		}
-		return "minecraft:log", 3
-	case AcaciaWood():
-		if l.Stripped {
-			return "minecraft:stripped_acacia_log", 0
-		}
-		return "minecraft:log2", 0
-	case DarkOakWood():
-		if l.Stripped {
-			return "minecraft:stripped_dark_oak_log", 0
-		}
-		return "minecraft:log2", 1
+	case OakWood(), SpruceWood(), BirchWood(), JungleWood(), AcaciaWood(), DarkOakWood():
+		return "minecraft:stripped_" + l.Wood.String() + "_log", 0
+	case CrimsonWood(), WarpedWood():
+		return "minecraft:stripped_" + l.Wood.String() + "_stem", 0
 	}
 	panic("invalid wood type")
 }
@@ -95,38 +82,26 @@ func (l Log) EncodeBlock() (name string, properties map[string]interface{}) {
 			return "minecraft:log", map[string]interface{}{"pillar_axis": l.Axis.String(), "old_log_type": l.Wood.String()}
 		case AcaciaWood(), DarkOakWood():
 			return "minecraft:log2", map[string]interface{}{"pillar_axis": l.Axis.String(), "new_log_type": l.Wood.String()}
+		case CrimsonWood(), WarpedWood():
+			return "minecraft:" + l.Wood.String() + "_stem", map[string]interface{}{"pillar_axis": l.Axis.String()}
 		}
 	}
 	switch l.Wood {
-	case OakWood():
-		return "minecraft:stripped_oak_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
-	case SpruceWood():
-		return "minecraft:stripped_spruce_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
-	case BirchWood():
-		return "minecraft:stripped_birch_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
-	case JungleWood():
-		return "minecraft:stripped_jungle_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
-	case AcaciaWood():
-		return "minecraft:stripped_acacia_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
-	case DarkOakWood():
-		return "minecraft:stripped_dark_oak_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
+	case OakWood(), SpruceWood(), BirchWood(), JungleWood(), AcaciaWood(), DarkOakWood():
+		return "minecraft:stripped_" + l.Wood.String() + "_log", map[string]interface{}{"pillar_axis": l.Axis.String()}
+	case CrimsonWood(), WarpedWood():
+		return "minecraft:stripped_" + l.Wood.String() + "_stem", map[string]interface{}{"pillar_axis": l.Axis.String()}
 	}
 	panic("invalid wood type")
 }
 
 // allLogs returns a list of all possible log states.
 func allLogs() (logs []world.Block) {
-	f := func(axis cube.Axis, stripped bool) {
-		logs = append(logs, Log{Axis: axis, Stripped: stripped, Wood: OakWood()})
-		logs = append(logs, Log{Axis: axis, Stripped: stripped, Wood: SpruceWood()})
-		logs = append(logs, Log{Axis: axis, Stripped: stripped, Wood: BirchWood()})
-		logs = append(logs, Log{Axis: axis, Stripped: stripped, Wood: JungleWood()})
-		logs = append(logs, Log{Axis: axis, Stripped: stripped, Wood: AcaciaWood()})
-		logs = append(logs, Log{Axis: axis, Stripped: stripped, Wood: DarkOakWood()})
-	}
-	for axis := cube.Axis(0); axis < 3; axis++ {
-		f(axis, true)
-		f(axis, false)
+	for _, w := range WoodTypes() {
+		for axis := cube.Axis(0); axis < 3; axis++ {
+			logs = append(logs, Log{Axis: axis, Stripped: true, Wood: w})
+			logs = append(logs, Log{Axis: axis, Stripped: false, Wood: w})
+		}
 	}
 	return
 }
