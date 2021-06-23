2d713c72bc064833fd102fe48afe049ecf5d5c67
T14Raptor <t14raptor@gmail.com>
2021-06-22 23:02:58 -0400

    block/register.go: Register prismarine block items.

--- server/block/register.go
+++ server/block/register.go
@@ -347,6 +347,9 @@ func init() {
 	world.RegisterItem(MossCarpet{})
 	world.RegisterItem(SporeBlossom{})
 	world.RegisterItem(Dripstone{})
+	world.RegisterItem(Prismarine{Type: NormalPrismarine()})
+	world.RegisterItem(Prismarine{Type: DarkPrismarine()})
+	world.RegisterItem(Prismarine{Type: BrickPrismarine()})
 
 	world.RegisterItem(item.Bucket{Content: Water{}})
 	world.RegisterItem(item.Bucket{Content: Lava{}})
