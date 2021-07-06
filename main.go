e16ee4a581e9968e8c78837b618f03d90be41af7
Whetu Paitai <56762410+FETXU@users.noreply.github.com>
2021-07-06 14:16:24 +1200

    Update main.go
    Implemented non-flat world generation (fixes #136)
    Utilizes extreme purlin noise generator.

--- main.go
+++ main.go
@@ -1,59 +1,62 @@
 package main
 
 import (
-	"fmt"
-	"github.com/df-mc/dragonfly/server"
-	"github.com/df-mc/dragonfly/server/player/chat"
-	"github.com/pelletier/go-toml"
-	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	"os"
+    "fmt"
+    "github.com/df-mc/dragonfly/server"
+    "github.com/df-mc/dragonfly/server/block"
+    "github.com/df-mc/dragonfly/server/block/cube"
+    "github.com/df-mc/dragonfly/server/player/chat"
+    "github.com/pelletier/go-toml"
+    "github.com/sirupsen/logrus"
+    "io/ioutil"
+    "os"
 )
 
 func main() {
-	log := logrus.New()
-	log.Formatter = &logrus.TextFormatter{ForceColors: true}
-	log.Level = logrus.DebugLevel
+    log := logrus.New()
+    log.Formatter = &logrus.TextFormatter{ForceColors: true}
+    log.Level = logrus.DebugLevel
 
-	chat.Global.Subscribe(chat.StdoutSubscriber{})
+    chat.Global.Subscribe(chat.StdoutSubscriber{})
 
-	config, err := readConfig()
-	if err != nil {
-		log.Fatalln(err)
-	}
+    config, err := readConfig()
+    if err != nil {
+        log.Fatalln(err)
+    }
 
-	srv := server.New(&config, log)
-	srv.CloseOnProgramEnd()
-	if err := srv.Start(); err != nil {
-		log.Fatalln(err)
-	}
+    srv := server.New(&config, log)
+    srv.CloseOnProgramEnd()
+    if err := srv.Start(); err != nil {
+        log.Fatalln(err)
+    }
+    srv.World().SetBlock(cube.Pos{0, 4, 0}, block.Grass{})
 
-	for {
-		if _, err := srv.Accept(); err != nil {
-			return
-		}
-	}
+    for {
+        if _, err := srv.Accept(); err != nil {
+            return
+        }
+    }
 }
 
 // readConfig reads the configuration from the config.toml file, or creates the file if it does not yet exist.
 func readConfig() (server.Config, error) {
-	c := server.DefaultConfig()
-	if _, err := os.Stat("config.toml"); os.IsNotExist(err) {
-		data, err := toml.Marshal(c)
-		if err != nil {
-			return c, fmt.Errorf("failed encoding default config: %v", err)
-		}
-		if err := ioutil.WriteFile("config.toml", data, 0644); err != nil {
-			return c, fmt.Errorf("failed creating config: %v", err)
-		}
-		return c, nil
-	}
-	data, err := ioutil.ReadFile("config.toml")
-	if err != nil {
-		return c, fmt.Errorf("error reading config: %v", err)
-	}
-	if err := toml.Unmarshal(data, &c); err != nil {
-		return c, fmt.Errorf("error decoding config: %v", err)
-	}
-	return c, nil
+    c := server.DefaultConfig()
+    if _, err := os.Stat("config.toml"); os.IsNotExist(err) {
+        data, err := toml.Marshal(c)
+        if err != nil {
+            return c, fmt.Errorf("failed encoding default config: %v", err)
+        }
+        if err := ioutil.WriteFile("config.toml", data, 0644); err != nil {
+            return c, fmt.Errorf("failed creating config: %v", err)
+        }
+        return c, nil
+    }
+    data, err := ioutil.ReadFile("config.toml")
+    if err != nil {
+        return c, fmt.Errorf("error reading config: %v", err)
+    }
+    if err := toml.Unmarshal(data, &c); err != nil {
+        return c, fmt.Errorf("error decoding config: %v", err)
+    }
+    return c, nil
 }
