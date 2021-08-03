b5188b0ca809be409b06807a59eb2a42abfeff94
Sandertv <st.ten.veldhuis@gmail.com>
2021-08-03 20:50:47 +0200

    internal/logger.go: Added documentation.

--- server/internal/logger.go
+++ server/internal/logger.go
@@ -1,5 +1,7 @@
 package internal
 
+// Logger is a type that is used to log errors and info to throughout Dragonfly. Any logger implementation that
+// implements Logger may be used by passing it to server.New.
 type Logger interface {
 	Debugf(format string, v ...interface{})
 	Infof(format string, v ...interface{})
