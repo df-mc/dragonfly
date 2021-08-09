65854692ea31f2a25b114e8302f7f9bf4c3d2521
Sandertv <st.ten.veldhuis@gmail.com>
2021-08-09 12:30:43 +0200

    chunk/decode.go: Re-use more code between disk and network decoding.

--- server/world/chunk/decode.go
+++ server/world/chunk/decode.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/sandertv/gophertunnel/minecraft/nbt"
-	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
 
 // StateToRuntimeID must hold a function to convert a name and its state properties to a runtime ID.
@@ -15,38 +14,16 @@ var StateToRuntimeID func(name string, properties map[string]interface{}) (runti
 // returned is nil and the error non-nil.
 // The sub chunk count passed must be that found in the LevelChunk packet.
 //noinspection GoUnusedExportedFunction
-func NetworkDecode(airRuntimeId uint32, data []byte, subChunkCount int) (*Chunk, error) {
-	c, buf := New(airRuntimeId), bytes.NewBuffer(data)
+func NetworkDecode(air uint32, data []byte, subChunkCount int) (*Chunk, error) {
+	var (
+		c   = New(air)
+		buf = bytes.NewBuffer(data)
+		err error
+	)
 	for y := 0; y < subChunkCount; y++ {
-		ver, err := buf.ReadByte()
+		c.sub[y], err = decodeSubChunk(buf, air, NetworkEncoding)
 		if err != nil {
-			return nil, fmt.Errorf("error reading version: %w", err)
-		}
-		c.sub[y] = NewSubChunk(airRuntimeId)
-		switch ver {
-		default:
-			return nil, fmt.Errorf("unknown sub chunk version %v: can't decode", ver)
-		case 1:
-			// Version 1 only has one layer for each sub chunk, but uses the format with palettes.
-			storage, err := networkDecodeBlockStorage(buf)
-			if err != nil {
-				return nil, err
-			}
-			c.sub[y].storages = append(c.sub[y].storages, storage)
-		case 8:
-			// Version 8 allows up to 256 layers for one sub chunk.
-			storageCount, err := buf.ReadByte()
-			if err != nil {
-				return nil, fmt.Errorf("error reading storage count: %w", err)
-			}
-			c.sub[y].storages = make([]*BlockStorage, storageCount)
-
-			for i := byte(0); i < storageCount; i++ {
-				c.sub[y].storages[i], err = networkDecodeBlockStorage(buf)
-				if err != nil {
-					return nil, err
-				}
-			}
+			return nil, err
 		}
 	}
 	if _, err := buf.Read(c.biomes[:]); err != nil {
@@ -73,7 +50,10 @@ func DiskDecode(data SerialisedData) (*Chunk, error) {
 		panic("cannot find air runtime ID")
 	}
 
-	c := New(air)
+	var (
+		c   = New(air)
+		err error
+	)
 	copy(c.biomes[:], data.Data2D[512:])
 
 	for y, sub := range data.SubChunks {
@@ -81,94 +61,53 @@ func DiskDecode(data SerialisedData) (*Chunk, error) {
 			// No data for this sub chunk.
 			continue
 		}
-		buf := bytes.NewBuffer(sub)
-		ver, err := buf.ReadByte()
+		c.sub[y], err = decodeSubChunk(bytes.NewBuffer(sub), air, DiskEncoding)
 		if err != nil {
-			return nil, fmt.Errorf("error reading version: %w", err)
-		}
-		c.sub[y] = NewSubChunk(air)
-		switch ver {
-		default:
-			return nil, fmt.Errorf("unknown sub chunk version %v: can't decode", ver)
-		case 1:
-			// Version 1 only has one layer for each sub chunk, but uses the format with palettes.
-			storage, err := diskDecodeBlockStorage(buf)
-			if err != nil {
-				return nil, err
-			}
-			c.sub[y].storages = append(c.sub[y].storages, storage)
-		case 8:
-			// Version 8 allows up to 256 layers for one sub chunk.
-			storageCount, err := buf.ReadByte()
-			if err != nil {
-				return nil, fmt.Errorf("error reading storage count: %w", err)
-			}
-			c.sub[y].storages = make([]*BlockStorage, storageCount)
-
-			for i := byte(0); i < storageCount; i++ {
-				c.sub[y].storages[i], err = diskDecodeBlockStorage(buf)
-				if err != nil {
-					return nil, err
-				}
-			}
+			return nil, err
 		}
 	}
 	return c, nil
 }
 
-// networkDecodeBlockStorage decodes a block storage from the buffer passed, assuming it holds data for a
-// network encoded block storage, and returns it if successful.
-func networkDecodeBlockStorage(buf *bytes.Buffer) (*BlockStorage, error) {
-	blockSize, err := buf.ReadByte()
+// decodeSubChunk decodes a SubChunk from a bytes.Buffer. The Encoding passed defines how the block storages of the
+// SubChunk are decoded.
+func decodeSubChunk(buf *bytes.Buffer, air uint32, e Encoding) (*SubChunk, error) {
+	ver, err := buf.ReadByte()
 	if err != nil {
-		return nil, fmt.Errorf("error reading block size: %w", err)
+		return nil, fmt.Errorf("error reading version: %w", err)
 	}
-	blockSize >>= 1
-
-	// blocksPerUint32 is the amount of blocks that may be stored in a single uint32.
-	blocksPerUint32 := 32 / int(blockSize)
-
-	// uint32Count is the amount of uint32s required to store all blocks: 4096 blocks need to be stored in
-	// total.
-	uint32Count := 4096 / blocksPerUint32
-
-	if paletteSize(blockSize).padded() {
-		// We've got one of the padded sizes, so the block storage has another uint32 to be able to store
-		// every block.
-		uint32Count++
-	}
-
-	uint32s := make([]uint32, uint32Count)
-	data := buf.Next(uint32Count * 4)
-	if len(data) != uint32Count*4 {
-		return nil, fmt.Errorf("cannot read block storage: not enough block data present: expected %v bytes, got %v", uint32Count*4, len(data))
-	}
-	for i := 0; i < uint32Count; i++ {
-		// Explicitly don't use the binary package to greatly improve performance of reading the uint32s.
-		uint32s[i] = uint32(data[i*4]) | uint32(data[i*4+1])<<8 | uint32(data[i*4+2])<<16 | uint32(data[i*4+3])<<24
-	}
-
-	var paletteCount int32
-	if err := protocol.Varint32(buf, &paletteCount); err != nil {
-		return nil, fmt.Errorf("error reading palette entry count: %w", err)
-	}
-	if paletteCount <= 0 {
-		return nil, fmt.Errorf("invalid palette entry count %v", paletteCount)
-	}
-
-	blocks, temp := make([]uint32, paletteCount), int32(0)
-	for i := int32(0); i < paletteCount; i++ {
-		if err := protocol.Varint32(buf, &temp); err != nil {
-			return nil, fmt.Errorf("error decoding palette entry: %w", err)
+	sub := NewSubChunk(air)
+	switch ver {
+	default:
+		return nil, fmt.Errorf("unknown sub chunk version %v: can't decode", ver)
+	case 1:
+		// Version 1 only has one layer for each sub chunk, but uses the format with palettes.
+		storage, err := decodeBlockStorage(buf, e)
+		if err != nil {
+			return nil, err
 		}
-		blocks[i] = uint32(temp)
+		sub.storages = append(sub.storages, storage)
+	case 8:
+		// Version 8 allows up to 256 layers for one sub chunk.
+		storageCount, err := buf.ReadByte()
+		if err != nil {
+			return nil, fmt.Errorf("error reading storage count: %w", err)
+		}
+		sub.storages = make([]*BlockStorage, storageCount)
+
+		for i := byte(0); i < storageCount; i++ {
+			sub.storages[i], err = decodeBlockStorage(buf, e)
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
-	return newBlockStorage(uint32s, &Palette{blockRuntimeIDs: blocks, size: paletteSize(blockSize)}), nil
+	return sub, nil
 }
 
-// diskDecodeBlockStorage decodes a block storage from the buffer passed. If not successful, an error is
-// returned.
-func diskDecodeBlockStorage(buf *bytes.Buffer) (*BlockStorage, error) {
+// decodeBlockStorage decodes a block storage from a bytes.Buffer. The Encoding passed is used to read either a network
+// or disk block storage.
+func decodeBlockStorage(buf *bytes.Buffer, e Encoding) (*BlockStorage, error) {
 	blockSize, err := buf.ReadByte()
 	if err != nil {
 		return nil, fmt.Errorf("error reading block size: %w", err)
@@ -189,43 +128,16 @@ func diskDecodeBlockStorage(buf *bytes.Buffer) (*BlockStorage, error) {
 	}
 
 	uint32s := make([]uint32, uint32Count)
+	byteCount := uint32Count * 4
 
-	data := buf.Next(uint32Count * 4)
-	if len(data) != uint32Count*4 {
+	data := buf.Next(byteCount)
+	if len(data) != byteCount {
 		return nil, fmt.Errorf("cannot read block storage: not enough block data present: expected %v bytes, got %v", uint32Count*4, len(data))
 	}
-
 	for i := 0; i < uint32Count; i++ {
 		// Explicitly don't use the binary package to greatly improve performance of reading the uint32s.
 		uint32s[i] = uint32(data[i*4]) | uint32(data[i*4+1])<<8 | uint32(data[i*4+2])<<16 | uint32(data[i*4+3])<<24
 	}
-
-	// The next 4 bytes are an LE int32, but we simply read it and decode the int32 ourselves, as it's much
-	// faster here.
-	data = buf.Next(4)
-	if len(data) != 4 {
-		return nil, fmt.Errorf("cannot read palette entry count: expected 4 bytes, got %v", len(data))
-	}
-	paletteCount := uint32(data[0]) | uint32(data[1])<<8 | uint32(data[2])<<16 | uint32(data[3])<<24
-
-	blocks := make([]blockEntry, paletteCount)
-
-	dec := nbt.NewDecoderWithEncoding(buf, nbt.LittleEndian)
-
-	// There are paletteCount NBT tags that represent unique blocks.
-	for i := uint32(0); i < paletteCount; i++ {
-		if err := dec.Decode(&blocks[i]); err != nil {
-			return nil, fmt.Errorf("error decoding block: %w", err)
-		}
-	}
-
-	palette := newPalette(paletteSize(blockSize), make([]uint32, paletteCount))
-	for i, b := range blocks {
-		var ok bool
-		palette.blockRuntimeIDs[i], ok = StateToRuntimeID(b.Name, b.State)
-		if !ok {
-			return nil, fmt.Errorf("cannot get runtime ID of block state %v{%+v}", b.Name, b.State)
-		}
-	}
-	return newBlockStorage(uint32s, palette), nil
+	p, err := e.decodePalette(buf, paletteSize(blockSize))
+	return newBlockStorage(uint32s, p), err
 }
--- server/world/chunk/encoding.go
+++ server/world/chunk/encoding.go
@@ -2,6 +2,8 @@ package chunk
 
 import (
 	"bytes"
+	"encoding/binary"
+	"fmt"
 	"github.com/sandertv/gophertunnel/minecraft/nbt"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 )
@@ -11,6 +13,7 @@ import (
 type Encoding interface {
 	encoding() nbt.Encoding
 	encodePalette(buf *bytes.Buffer, p *Palette)
+	decodePalette(buf *bytes.Buffer, blockSize paletteSize) (*Palette, error)
 	network() byte
 	data2D(c *Chunk) []byte
 }
@@ -22,6 +25,7 @@ var DiskEncoding diskEncoding
 // NetworkEncoding is the Encoding used for sending a Chunk over network. It does not use NBT and writes varints.
 var NetworkEncoding networkEncoding
 
+// diskEncoding implements the Chunk encoding for writing to disk.
 type diskEncoding struct{}
 
 func (diskEncoding) network() byte          { return 0 }
@@ -41,7 +45,33 @@ func (diskEncoding) encodePalette(buf *bytes.Buffer, p *Palette) {
 		_ = enc.Encode(b)
 	}
 }
+func (diskEncoding) decodePalette(buf *bytes.Buffer, blockSize paletteSize) (*Palette, error) {
+	// The next 4 bytes are an LE int32, but we simply read it and decode the int32 ourselves, as it's much
+	// faster here.
+	data := buf.Next(4)
+	if len(data) != 4 {
+		return nil, fmt.Errorf("cannot read palette entry count: expected 4 bytes, got %v", len(data))
+	}
+	var (
+		paletteCount = binary.LittleEndian.Uint32(data)
+		palette      = newPalette(blockSize, make([]uint32, paletteCount))
+		dec          = nbt.NewDecoderWithEncoding(buf, nbt.LittleEndian)
+		e            blockEntry
+		ok           bool
+	)
+	for i := uint32(0); i < paletteCount; i++ {
+		if err := dec.Decode(&e); err != nil {
+			return nil, fmt.Errorf("error decoding block: %w", err)
+		}
+		palette.blockRuntimeIDs[i], ok = StateToRuntimeID(e.Name, e.State)
+		if !ok {
+			return nil, fmt.Errorf("cannot get runtime ID of block state %v{%+v}", e.Name, e.State)
+		}
+	}
+	return palette, nil
+}
 
+// networkEncoding implements the Chunk encoding for sending over network.
 type networkEncoding struct{}
 
 func (networkEncoding) network() byte          { return 1 }
@@ -52,4 +82,22 @@ func (networkEncoding) encodePalette(buf *bytes.Buffer, p *Palette) {
 	for _, runtimeID := range p.blockRuntimeIDs {
 		_ = protocol.WriteVarint32(buf, int32(runtimeID))
 	}
+}
+func (networkEncoding) decodePalette(buf *bytes.Buffer, blockSize paletteSize) (*Palette, error) {
+	var paletteCount int32
+	if err := protocol.Varint32(buf, &paletteCount); err != nil {
+		return nil, fmt.Errorf("error reading palette entry count: %w", err)
+	}
+	if paletteCount <= 0 {
+		return nil, fmt.Errorf("invalid palette entry count %v", paletteCount)
+	}
+
+	blocks, temp := make([]uint32, paletteCount), int32(0)
+	for i := int32(0); i < paletteCount; i++ {
+		if err := protocol.Varint32(buf, &temp); err != nil {
+			return nil, fmt.Errorf("error decoding palette entry: %w", err)
+		}
+		blocks[i] = uint32(temp)
+	}
+	return &Palette{blockRuntimeIDs: blocks, size: blockSize}, nil
 }
