312316787a28486456c60e28bbeae4d710b675ed
Lucaskyy <contact@sculas.xyz>
2021-08-11 16:53:59 +0200

    moved compiler directive code to entity_internal

--- server/block/fire.go
+++ server/block/fire.go
@@ -6,10 +6,7 @@ import (
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/entity"
 	"github.com/df-mc/dragonfly/server/entity/damage"
-	"github.com/df-mc/dragonfly/server/entity/physics"
 	"github.com/df-mc/dragonfly/server/world"
-	"github.com/go-gl/mathgl/mgl64"
-	"math"
 	"math/rand"
 	"time"
 	_ "unsafe" // Imported for compiler directives.
@@ -36,8 +33,8 @@ func FlammableBlock(block world.Block) bool {
 	return false
 }
 
-// neighboursFlammable returns true if one a block adjacent to the passed position is flammable.
-func neighboursFlammable(pos cube.Pos, w *world.World) bool {
+// NeighboursFlammable returns true if one a block adjacent to the passed position is flammable.
+func NeighboursFlammable(pos cube.Pos, w *world.World) bool {
 	for i := cube.Face(0); i < 6; i++ {
 		if FlammableBlock(w.Block(pos.Side(i))) {
 			return true
@@ -102,7 +99,7 @@ func (f Fire) tick(pos cube.Pos, w *world.World, r *rand.Rand) {
 				w.BreakBlockWithoutParticles(pos)
 				return
 			}
-			if !neighboursFlammable(pos, w) {
+			if !NeighboursFlammable(pos, w) {
 				if !w.Block(pos.Side(cube.FaceDown)).Model().FaceSolid(pos, cube.FaceUp, w) || f.Age > 3 {
 					w.BreakBlockWithoutParticles(pos)
 				}
@@ -189,7 +186,7 @@ func (f Fire) RandomTick(pos cube.Pos, w *world.World, r *rand.Rand) {
 // NeighbourUpdateTick ...
 func (f Fire) NeighbourUpdateTick(pos, neighbour cube.Pos, w *world.World) {
 	below := w.Block(pos.Side(cube.FaceDown))
-	if !below.Model().FaceSolid(pos, cube.FaceUp, w) && (!neighboursFlammable(pos, w) || f.Type == SoulFire()) {
+	if !below.Model().FaceSolid(pos, cube.FaceUp, w) && (!NeighboursFlammable(pos, w) || f.Type == SoulFire()) {
 		w.BreakBlockWithoutParticles(pos)
 	} else {
 		switch below.(type) {
@@ -237,78 +234,4 @@ func allFire() (b []world.Block) {
 		b = append(b, Fire{Age: i, Type: SoulFire()})
 	}
 	return
-}
-
-//go:linkname block_setBlocksOnFire github.com/df-mc/dragonfly/server/entity.setBlocksOnFire
-//noinspection ALL
-var block_setBlocksOnFire func(w *world.World, lPos mgl64.Vec3)
-
-func init() {
-	block_setBlocksOnFire = func(w *world.World, lPos mgl64.Vec3) {
-		_, isNormal := w.Difficulty().(world.DifficultyNormal)
-		_, isHard := w.Difficulty().(world.DifficultyHard)
-		if isNormal || isHard { // difficulty >= 2
-			bPos := cube.PosFromVec3(lPos)
-			b := w.Block(bPos)
-			_, isAir := b.(Air)
-			_, isTallGrass := b.(TallGrass)
-			if isAir || isTallGrass {
-				below := w.Block(bPos.Side(cube.FaceDown))
-				if below.Model().FaceSolid(bPos, cube.FaceUp, w) || neighboursFlammable(bPos, w) {
-					w.PlaceBlock(bPos, Fire{})
-				}
-			}
-		}
-	}
-}
-
-// LIGHTNING HANDLING
-
-//go:linkname world_performThunder github.com/df-mc/dragonfly/server/world.performThunder
-//noinspection ALL
-var world_performThunder func(w *world.World, pos world.ChunkPos)
-
-func init() {
-	world_performThunder = func(w *world.World, pos world.ChunkPos) {
-		LCG := int32(w.GetUpdateLGC() >> 2)
-
-		chunkX, chunkZ := pos.X(), pos.Z()
-		vec := adjustPosToNearbyEntities(w, mgl64.Vec3{float64(chunkX + (LCG & 0xf)), 0, float64(chunkZ + (LCG >> 8 & 0xf))})
-
-		blockType := w.Block(cube.Pos{int(math.Floor(vec.X())) & 0xf, int(math.Floor(vec.Y())), int(math.Floor(vec.Z())) & 0xf})
-
-		_, tallGrass := blockType.(TallGrass)
-		_, flowingWater := blockType.(Water)
-		if !tallGrass && !flowingWater {
-			vec = vec.Add(mgl64.Vec3{0, 1, 0})
-		}
-
-		w.AddEntity(entity.NewLightning(vec))
-	}
-}
-
-func canBlockSeeSky(w *world.World, pos mgl64.Vec3) bool {
-	return w.HighestBlock(int(pos.X()), int(pos.Z())) < int(pos.Y())
-}
-
-func adjustPosToNearbyEntities(w *world.World, pos mgl64.Vec3) mgl64.Vec3 {
-	pos = mgl64.Vec3{pos.X(), float64(w.HighestBlock(int(math.Floor(pos.X())), int(math.Floor(pos.Z())))), pos.Z()}
-	aabb := physics.NewAABB(pos, mgl64.Vec3{pos.X(), 255, pos.Z()}).Extend(mgl64.Vec3{3, 3, 3})
-	var list []mgl64.Vec3
-
-	for _, e := range w.CollidingEntities(aabb) {
-		if l, ok := e.(entity.Living); ok && l.Health() <= 0 && canBlockSeeSky(w, l.Position()) {
-			list = append(list, l.Position())
-		}
-	}
-
-	if len(list) > 0 {
-		return list[rand.Intn(len(list))]
-	} else {
-		if pos.Y() == -1 {
-			pos = pos.Add(mgl64.Vec3{0, 2, 0})
-		}
-
-		return pos
-	}
 }
