9df0f8e36db6bffc026d9e833688d8d81e8f39e1
T14Raptor <t14raptor@gmail.com>
2021-08-11 14:56:29 -0400

    player/player.go: Fixed OnGround() always returning true and fixed EntityCollide getting called when it shouldn't.

--- server/player/player.go
+++ server/player/player.go
@@ -1518,7 +1518,8 @@ func (p *Player) Move(deltaPos mgl64.Vec3) {
 
 		p.pos.Store(res)
 
-		p.checkCollisions()
+		p.checkBlockCollisions()
+		p.onGround.Store(p.checkOnGround())
 
 		p.updateFallState(deltaPos[1])
 
@@ -1640,7 +1641,10 @@ func (p *Player) Tick(current int64) {
 			p.AddEffect(effect.New(effect.WaterBreathing{}, 1, time.Second*10))
 		}
 	}
-	p.checkCollisions()
+
+	p.checkBlockCollisions()
+	p.onGround.Store(p.checkOnGround())
+
 	p.tickFood()
 	p.effects.Tick(p)
 	if p.Position()[1] < cube.MinY && p.GameMode().AllowsTakingDamage() && current%10 == 0 {
@@ -1707,11 +1711,37 @@ func (p *Player) starve() {
 	}
 }
 
-// checkCollisions checks the player's block collisions, including whether the player
-// is currently considered to be on the ground or not.
-func (p *Player) checkCollisions() {
-	var onGround bool
+// checkCollisions checks the player's block collisions.
+func (p *Player) checkBlockCollisions() {
+	w := p.World()
+	pos := p.Position()
 
+	aabb := p.AABB().Translate(p.Position())
+	grown := aabb.Grow(0.25)
+	min, max := grown.Min(), grown.Max()
+	minX, minY, minZ := int(math.Floor(min[0])), int(math.Floor(min[1])), int(math.Floor(min[2]))
+	maxX, maxY, maxZ := int(math.Ceil(max[0])), int(math.Ceil(max[1])), int(math.Ceil(max[2]))
+
+	for y := minY; y <= maxY; y++ {
+		for x := minX; x <= maxX; x++ {
+			for z := minZ; z <= maxZ; z++ {
+				blockPos := cube.Pos{x, y, z}
+				b := w.Block(blockPos)
+				for _, bb := range b.Model().AABB(blockPos, w) {
+					if aabb.IntersectsWith(bb.Translate(pos)) {
+						if collide, ok := b.(block.EntityCollider); ok {
+							collide.EntityCollide(p)
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+// checkOnGround checks if the player is currently considered to be on the ground.
+func (p *Player) checkOnGround() bool {
 	w := p.World()
 	pos := p.Position()
 	pAABB := p.AABB().Translate(pos)
@@ -1722,22 +1752,16 @@ func (p *Player) checkCollisions() {
 			for y := pos[1] - 1; y < pos[1]+1; y++ {
 				bPos := cube.PosFromVec3(mgl64.Vec3{x, y, z})
 				b := w.Block(bPos)
-				if !onGround {
-					aabbList := b.Model().AABB(bPos, w)
-					for _, aabb := range aabbList {
-						if aabb.GrowVec3(mgl64.Vec3{0, 0.05}).Translate(bPos.Vec3()).IntersectsWith(pAABB) {
-							onGround = true
-							p.onGround.Store(onGround)
-						}
+				aabbList := b.Model().AABB(bPos, p.World())
+				for _, aabb := range aabbList {
+					if aabb.GrowVec3(mgl64.Vec3{0, 0.05}).Translate(bPos.Vec3()).IntersectsWith(pAABB) {
+						return true
 					}
-				}
-
-				if collide, ok := b.(block.EntityCollider); ok {
-					collide.EntityCollide(p)
 				}
 			}
 		}
 	}
+	return false
 }
 
 // AABB returns the axis aligned bounding box of the player.
