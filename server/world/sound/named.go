7b995f6b756609b12389860877fceb848149f92a
tigerza117 <noppawanza1171@gmail.com>
2021-10-12 06:53:03 +0700

    world: Changed Named field and type.

--- server/world/sound/named.go
+++ server/world/sound/named.go
@@ -2,14 +2,14 @@ package sound
 
 // Named is a sound from resource pack, You can use custom sound from resource packs.
 type Named struct {
-	// SoundName is the name of the sound to play.
-	SoundName string
+	// Name is the name of the sound to play.
+	Name string
 	// Volume is the relative volume of the sound to play. It will be less loud for the player if it is
 	// farther away from the position of the sound.
-	Volume float32
+	Volume float64
 	// Pitch is the pitch of the sound to play. Some sounds completely ignore this field, whereas others use
 	// it to specify the pitch as the field is intended.
-	Pitch float32
+	Pitch float64
 
 	sound
 }
