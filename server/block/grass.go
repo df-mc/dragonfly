a8c678901f97c009dda7670a142079fc27afa968
JustTal <justtal@protonmail.com>
2021-10-16 18:30:17 -0500

    Removed plant selection and instead made it a package level variable

--- server/block/grass.go
+++ server/block/grass.go
@@ -11,6 +11,28 @@ type Grass struct {
 	solid
 }
 
+// plantSelection are the plants that are picked from when a bone meal is attempted.
+var plantSelection = []world.Block{
+	Flower{Type: OxeyeDaisy()},
+	Flower{Type: PinkTulip()},
+	Flower{Type: Cornflower()},
+	Flower{Type: WhiteTulip()},
+	Flower{Type: RedTulip()},
+	Flower{Type: OrangeTulip()},
+	Flower{Type: Dandelion()},
+	Flower{Type: Poppy()},
+}
+
+// init adds extra variants of TallGrass to the plant selection.
+func init() {
+	for i := 0; i < 8; i++ {
+		plantSelection = append(plantSelection, TallGrass{Type: Fern()})
+	}
+	for i := 0; i < 12; i++ {
+		plantSelection = append(plantSelection, TallGrass{Type: NormalGrass()})
+	}
+}
+
 // SoilFor ...
 func (g Grass) SoilFor(block world.Block) bool {
 	switch block.(type) {
@@ -56,12 +78,11 @@ func (g Grass) RandomTick(pos cube.Pos, w *world.World, r *rand.Rand) {
 
 // BoneMeal ...
 func (g Grass) BoneMeal(pos cube.Pos, w *world.World) bool {
-	plants := plantSelection() // TODO: Differ plants based on biomes when a proper implementation of it is made.
 	for c := 0; c < 14; c++ {
 		x := randWithinRange(pos.X()-3, pos.X()+3)
 		z := randWithinRange(pos.Z()-3, pos.Z()+3)
 		if (w.Block(cube.Pos{x, pos.Y() + 1, z}) == Air{}) && (w.Block(cube.Pos{x, pos.Y(), z}) == Grass{}) {
-			w.SetBlock(cube.Pos{x, pos.Y() + 1, z}, plants[randWithinRange(0, len(plants)-1)])
+			w.SetBlock(cube.Pos{x, pos.Y() + 1, z}, plantSelection[randWithinRange(0, len(plantSelection)-1)])
 		}
 	}
 
@@ -91,28 +112,6 @@ func (g Grass) Till() (world.Block, bool) {
 // Shovel ...
 func (g Grass) Shovel() (world.Block, bool) {
 	return DirtPath{}, true
-}
-
-// plantSelection returns the selection of plants that can be grown by bone meal.
-func plantSelection() []world.Block {
-	plants := []world.Block{
-		Flower{Type: OxeyeDaisy()},
-		Flower{Type: PinkTulip()},
-		Flower{Type: Cornflower()},
-		Flower{Type: WhiteTulip()},
-		Flower{Type: RedTulip()},
-		Flower{Type: OrangeTulip()},
-		Flower{Type: Dandelion()},
-		Flower{Type: Poppy()},
-	}
-	for i := 0; i < 8; i++ {
-		plants = append(plants, TallGrass{Type: Fern()})
-	}
-	for i := 0; i < 12; i++ {
-		plants = append(plants, TallGrass{Type: NormalGrass()})
-	}
-
-	return plants
 }
 
 // randWithinRange returns a random integer within a range.
