329bed98cc52e5ada640f6e3eb825bfc1c21af0b
JustTal <justtal@protonmail.com>
2021-11-20 11:50:02 -0600

    Ensure the width of each row is the same

--- server/item/recipe/dimensions.go
+++ server/item/recipe/dimensions.go
@@ -11,10 +11,15 @@ func DimensionsFrom(shape []string) (Dimensions, error) {
 	if height > 3 || height <= 0 {
 		return Dimensions{}, fmt.Errorf("shaped recipes may only have 1, 2 or 3 rows, not height")
 	}
-
 	width := len(shape[0])
 	if width > 3 || width <= 0 {
 		return Dimensions{}, fmt.Errorf("shaped recipes may only have 1, 2 or 3 columns, not width")
+	}
+
+	for _, row := range shape {
+		if len(row) != width {
+			return Dimensions{}, fmt.Errorf("shaped recipe rows must all have the same width (expected width, got %v)", len(row))
+		}
 	}
 	return Dimensions{width, height}, nil
 }
