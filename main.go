9654b31660a769a8e9d14fbbb6e0a15f9088c861
JustTal <justtal@protonmail.com>
2021-12-27 10:56:44 -0600

    Remove testing code.

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"github.com/df-mc/dragonfly/server"
 	"github.com/df-mc/dragonfly/server/player/chat"
-	"github.com/df-mc/dragonfly/server/world"
 	"github.com/pelletier/go-toml"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
-	"time"
 )
 
 func main() {
@@ -31,29 +29,8 @@ func main() {
 	}
 
 	for {
-		if p, err := srv.Accept(); err != nil {
+		if _, err := srv.Accept(); err != nil {
 			return
-		} else {
-			p.SetGameMode(world.GameModeSurvival)
-			go func() {
-				overworld := p.World()
-				nether, end := overworld.PortalDestinations()
-				time.Sleep(5 * time.Second)
-				if !p.Dead() {
-					nether.AddEntity(p)
-					p.Teleport(nether.Spawn().Vec3Centre())
-					time.Sleep(5 * time.Second)
-					if !p.Dead() {
-						end.AddEntity(p)
-						p.Teleport(end.Spawn().Vec3Centre())
-						time.Sleep(5 * time.Second)
-						if !p.Dead() {
-							overworld.AddEntity(p)
-							p.Teleport(overworld.Spawn().Vec3Centre())
-						}
-					}
-				}
-			}()
 		}
 	}
 }
