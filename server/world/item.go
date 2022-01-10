c2f2701c398dc59c7adeed6de8cdb5f55b575a1a
JustTal <justtal@protonmail.com>
2022-01-09 18:42:16 -0600

    Fix staticheck issues

--- server/world/item.go
+++ server/world/item.go
@@ -3,7 +3,9 @@ package world
 import (
 	_ "embed"
 	"fmt"
+	"github.com/df-mc/dragonfly/server/item/category"
 	"github.com/sandertv/gophertunnel/minecraft/nbt"
+	"image"
 )
 
 // Item represents an item that may be added to an inventory. It has a method to encode the item to an ID and
@@ -12,6 +14,18 @@ type Item interface {
 	// EncodeItem encodes the item to its Minecraft representation, which consists of a numerical ID and a
 	// metadata value.
 	EncodeItem() (name string, meta int16)
+}
+
+// CustomItem represents an item that is non-vanilla and requires a resource pack and extra steps to show it
+// to the client.
+type CustomItem interface {
+	Item
+	// Name is the name that will be displayed on the item to all clients.
+	Name() string
+	// Texture is the Image of the texture for this item.
+	Texture() image.Image
+	// Category is the category the item will be listed under in the creative inventory.
+	Category() category.Category
 }
 
 // RegisterItem registers an item with the ID and meta passed. Once registered, items may be obtained from an
@@ -49,6 +63,8 @@ var (
 	// items holds a list of all registered items, indexed using the itemHash created when calling
 	// Item.EncodeItem.
 	items = map[itemHash]Item{}
+	// customItems holds a list of all registered custom items.
+	customItems []CustomItem
 	// itemRuntimeIDsToNames holds a map to translate item runtime IDs to string IDs.
 	itemRuntimeIDsToNames = map[int32]string{}
 	// itemNamesToRuntimeIDs holds a map to translate item string IDs to runtime IDs.
@@ -106,4 +122,9 @@ func Items() []Item {
 		m = append(m, i)
 	}
 	return m
+}
+
+// CustomItems returns a slice of all registered custom items.
+func CustomItems() []CustomItem {
+	return customItems
 }
