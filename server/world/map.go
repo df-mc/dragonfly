cb9feca81f121ddafeafea1c6a01a3fe250d21dd
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-23 17:58:16 +0800

    Map interface

---
+++ server/world/map.go
@@ -0,0 +1,17 @@
+package world
+
+import (
+	"image/color"
+	"sync"
+
+	"github.com/df-mc/dragonfly/server/block/cube"
+)
+
+// registeredMaps is a map that uses ID as key and Map as value.
+var registeredMaps sync.Map
+
+type Map interface {
+	SetPixels([][]color.RGBA)
+	TrackEntity(Entity)
+	TrackBlock(cube.Pos)
+}
