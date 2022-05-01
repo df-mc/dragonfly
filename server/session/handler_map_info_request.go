4a43519b182f71c94b4771c43a41debf4f2e3b22
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-05-02 02:17:34 +0800

    Error if player does not have corresponding map item

--- server/session/handler_map_info_request.go
+++ server/session/handler_map_info_request.go
@@ -1,8 +1,11 @@
 package session
 
 import (
+	"fmt"
 	"sync"
 
+	"github.com/df-mc/dragonfly/server/item"
+	"github.com/df-mc/dragonfly/server/item/inventory"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
 
@@ -13,6 +16,26 @@ type MapInfoRequestHandler struct {
 
 // Handle ...
 func (h *MapInfoRequestHandler) Handle(p packet.Packet, s *Session) error {
-	// pk := p.(*packet.MapInfoRequest)
+	pk := p.(*packet.MapInfoRequest)
+
+	for _, inv := range []*inventory.Inventory{
+		s.inv,
+		s.offHand,
+		s.ui,
+		s.armour.Inventory(),
+	} {
+		if inv.ContainsItemFunc(1, func(stack item.Stack) bool {
+			if mapItem, ok := stack.Item().(item.MapInterface); ok {
+				return mapItem.GetMapID() == pk.MapID
+			}
+
+			return false // Item is not map.
+		}) {
+			break
+		}
+
+		return fmt.Errorf("client requests info of map %v while he does not have the corresponding map item in inventory, off hand inventory, UI inventory or armour inventory", pk.MapID)
+	}
+
 	return nil
 }
