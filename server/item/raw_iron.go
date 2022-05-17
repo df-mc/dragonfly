79f51f047eb2f5187feb6594209bb8a8b73a8659
DaPigGuy <mcpepig123@gmail.com>
2022-05-16 19:10:54 -0700

    added missing smelting info for raw ore items

--- server/item/raw_iron.go
+++ server/item/raw_iron.go
@@ -3,7 +3,12 @@ package item
 // RawIron is a raw metal resource obtained from mining iron ore.
 type RawIron struct{}
 
+// SmeltInfo ...
+func (RawIron) SmeltInfo() SmeltInfo {
+	return SmeltInfo{Product: NewStack(IronIngot{}, 1), Experience: 0.7, Ores: true}
+}
+
 // EncodeItem ...
-func (r RawIron) EncodeItem() (name string, meta int16) {
+func (RawIron) EncodeItem() (name string, meta int16) {
 	return "minecraft:raw_iron", 0
 }
