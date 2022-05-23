94b4fb3e0cae2529f247a2196468c8606019eac7
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2022-05-23 16:48:14 +1000

    Adding Cacti.

---
+++ server/block/cactus.go
@@ -0,0 +1,83 @@
+package block
+
+import (
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/block/model"
+	"github.com/df-mc/dragonfly/server/entity"
+	"github.com/df-mc/dragonfly/server/entity/damage"
+	"github.com/df-mc/dragonfly/server/world"
+	"github.com/df-mc/dragonfly/server/world/particle"
+	"math/rand"
+)
+
+// Cactus is a naturally occurring block found in deserts
+type Cactus struct {
+	solid
+
+	// Age is the groth state of cactus. values from 0 to 15
+	Age int
+}
+
+// NeighbourUpdateTick ...
+func (c Cactus) NeighbourUpdateTick(pos, _ cube.Pos, w *world.World) {
+	for _, face := range cube.HorizontalFaces() {
+		_, ok := w.Block(pos.Side(face)).(Air)
+		if !ok {
+			w.SetBlock(pos, nil, nil)
+			w.AddParticle(pos.Vec3Centre(), particle.BlockBreak{Block: c})
+			return
+		}
+	}
+	if w.Block(pos.Side(cube.FaceDown)) != c && !supportsVegetation(c, w.Block(pos.Side(cube.FaceDown))) {
+		w.SetBlock(pos, nil, nil)
+		w.AddParticle(pos.Vec3Centre(), particle.BlockBreak{Block: c})
+	}
+}
+
+// RandomTick ...
+func (c Cactus) RandomTick(pos cube.Pos, w *world.World, r *rand.Rand) {
+	if c.Age < 15 && r.Float64() < 0.01 {
+		abovePos := pos.Add(cube.Pos{0, 1})
+
+		switch w.Block(abovePos).(type) {
+		case Air:
+			c.Age++
+			w.SetBlock(abovePos, Cactus{Age: c.Age}, nil)
+		}
+	}
+}
+
+// EntityInside ...
+func (c Cactus) EntityInside(_ cube.Pos, _ *world.World, e world.Entity) {
+	if l, ok := e.(entity.Living); ok && !l.AttackImmune() {
+		l.Hurt(0.5, damage.SourceCactus{})
+	}
+}
+
+// BreakInfo ...
+func (c Cactus) BreakInfo() BreakInfo {
+	return newBreakInfo(0.4, alwaysHarvestable, nothingEffective, oneOf(c))
+}
+
+// EncodeItem ...
+func (c Cactus) EncodeItem() (name string, meta int16) {
+	return "minecraft:cactus", 0
+}
+
+// EncodeBlock ...
+func (c Cactus) EncodeBlock() (name string, properties map[string]any) {
+	return "minecraft:cactus", map[string]any{"age": int32(c.Age)}
+}
+
+// Model ...
+func (c Cactus) Model() world.BlockModel {
+	return model.Cactus{}
+}
+
+// allCactus returns all possible states of a cactus block.
+func allCactus() (b []world.Block) {
+	for i := 0; i < 16; i++ {
+		b = append(b, Cactus{Age: i})
+	}
+	return
+}
