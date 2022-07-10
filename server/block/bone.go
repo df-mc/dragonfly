17292335c97dc66c42b4b28fba7ea08a5e105b28
Sandertv <st.ten.veldhuis@gmail.com>
2022-07-10 14:50:58 +0200

    server/block: Renamed a bunch of blocks to remove the `Block` suffix. This suffix is against Go standards because the types are already accessed using `block.<Type>`, meaning this causes stuttering. In most places, we already didn't have this suffix, so this commit makes that consistent. CoralBlocks are the only exception which cannot be fixed, because there is already a `Coral` block.

--- server/block/bone.go
+++ server/block/bone.go
@@ -8,8 +8,8 @@ import (
 	"github.com/go-gl/mathgl/mgl64"
 )
 
-// BoneBlock is a decorative block that can face different directions.
-type BoneBlock struct {
+// Bone is a decorative block that can face different directions.
+type Bone struct {
 	solid
 
 	// Axis is the axis which the bone block faces.
@@ -17,12 +17,12 @@ type BoneBlock struct {
 }
 
 // Instrument ...
-func (b BoneBlock) Instrument() sound.Instrument {
+func (b Bone) Instrument() sound.Instrument {
 	return sound.Xylophone()
 }
 
 // UseOnBlock handles the rotational placing of bone blocks.
-func (b BoneBlock) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *world.World, user item.User, ctx *item.UseContext) (used bool) {
+func (b Bone) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *world.World, user item.User, ctx *item.UseContext) (used bool) {
 	pos, face, used = firstReplaceable(w, pos, face, b)
 	if !used {
 		return
@@ -34,24 +34,24 @@ func (b BoneBlock) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *wor
 }
 
 // BreakInfo ...
-func (b BoneBlock) BreakInfo() BreakInfo {
+func (b Bone) BreakInfo() BreakInfo {
 	return newBreakInfo(2, pickaxeHarvestable, pickaxeEffective, oneOf(b))
 }
 
 // EncodeItem ...
-func (b BoneBlock) EncodeItem() (name string, meta int16) {
+func (b Bone) EncodeItem() (name string, meta int16) {
 	return "minecraft:bone_block", 0
 }
 
 // EncodeBlock ...
-func (b BoneBlock) EncodeBlock() (name string, properties map[string]any) {
+func (b Bone) EncodeBlock() (name string, properties map[string]any) {
 	return "minecraft:bone_block", map[string]any{"pillar_axis": b.Axis.String(), "deprecated": int32(0)}
 }
 
 // allBoneBlock ...
 func allBoneBlock() (boneBlocks []world.Block) {
 	for _, axis := range cube.Axes() {
-		boneBlocks = append(boneBlocks, BoneBlock{Axis: axis})
+		boneBlocks = append(boneBlocks, Bone{Axis: axis})
 	}
 	return
 }
--- server/block/lapis.go
+++ server/block/lapis.go
@@ -4,24 +4,24 @@ import (
 	"github.com/df-mc/dragonfly/server/item"
 )
 
-// LapisBlock is a decorative mineral block that is crafted from lapis lazuli.
-type LapisBlock struct {
+// Lapis is a decorative mineral block that is crafted from lapis lazuli.
+type Lapis struct {
 	solid
 }
 
 // BreakInfo ...
-func (l LapisBlock) BreakInfo() BreakInfo {
+func (l Lapis) BreakInfo() BreakInfo {
 	return newBreakInfo(3, func(t item.Tool) bool {
 		return t.ToolType() == item.TypePickaxe && t.HarvestLevel() >= item.ToolTierStone.HarvestLevel
 	}, pickaxeEffective, oneOf(l))
 }
 
 // EncodeItem ...
-func (LapisBlock) EncodeItem() (name string, meta int16) {
+func (Lapis) EncodeItem() (name string, meta int16) {
 	return "minecraft:lapis_block", 0
 }
 
 // EncodeBlock ...
-func (LapisBlock) EncodeBlock() (string, map[string]any) {
+func (Lapis) EncodeBlock() (string, map[string]any) {
 	return "minecraft:lapis_block", nil
 }
