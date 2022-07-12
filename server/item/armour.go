7c21b8ec747fbafbf98e12a0788bb3b2cb44800f
Hashim <13991048+Prim69@users.noreply.github.com>
2022-07-12 04:00:04 -0400

    server/item: Added custom colours on leather armour (#547)

--- server/item/armour.go
+++ server/item/armour.go
@@ -1,19 +1,6 @@
 package item
 
-var (
-	// ArmourTierLeather is the ArmourTier of leather armour.
-	ArmourTierLeather = ArmourTier{BaseDurability: 55, Name: "leather"}
-	// ArmourTierGold is the ArmourTier of gold armour.
-	ArmourTierGold = ArmourTier{BaseDurability: 77, Name: "golden"}
-	// ArmourTierChain is the ArmourTier of chain armour.
-	ArmourTierChain = ArmourTier{BaseDurability: 166, Name: "chainmail"}
-	// ArmourTierIron is the ArmourTier of iron armour.
-	ArmourTierIron = ArmourTier{BaseDurability: 165, Name: "iron"}
-	// ArmourTierDiamond is the ArmourTier of diamond armour.
-	ArmourTierDiamond = ArmourTier{BaseDurability: 363, Toughness: 2, Name: "diamond"}
-	// ArmourTierNetherite is the ArmourTier of netherite armour.
-	ArmourTierNetherite = ArmourTier{BaseDurability: 408, Toughness: 3, KnockBackResistance: 0.1, Name: "netherite"}
-)
+import "image/color"
 
 type (
 	// Armour represents an item that may be worn as armour. Generally, these items provide armour points, which
@@ -31,17 +18,17 @@ type (
 		KnockBackResistance() float64
 	}
 	// ArmourTier represents the tier, or material, that a piece of armour is made of.
-	ArmourTier struct {
+	ArmourTier interface {
 		// BaseDurability is the base durability of armour with this tier. This is otherwise the durability of
 		// the helmet with this tier.
-		BaseDurability float64
+		BaseDurability() float64
 		// Toughness reduces the defense reduction caused by damage increases.
-		Toughness float64
+		Toughness() float64
 		// KnockBackResistance is a number from 0-1 that decides the amount of knock back force that is resisted
 		// upon being attacked. 1 knock back resistance point client-side translates to 10% knock back reduction.
-		KnockBackResistance float64
+		KnockBackResistance() float64
 		// Name is the name of the tier.
-		Name string
+		Name() string
 	}
 	// HelmetType is an Armour item that can be worn in the helmet slot.
 	HelmetType interface {
@@ -65,16 +52,67 @@ type (
 	}
 )
 
+// ArmourTierLeather is the ArmourTier of leather armour
+type ArmourTierLeather struct {
+	// Colour is the dyed colour of the armour.
+	Colour color.RGBA
+}
+
+func (ArmourTierLeather) BaseDurability() float64      { return 55 }
+func (ArmourTierLeather) Toughness() float64           { return 0 }
+func (ArmourTierLeather) KnockBackResistance() float64 { return 0 }
+func (ArmourTierLeather) Name() string                 { return "leather" }
+
+// ArmourTierGold is the ArmourTier of gold armour.
+type ArmourTierGold struct{}
+
+func (ArmourTierGold) BaseDurability() float64      { return 77 }
+func (ArmourTierGold) Toughness() float64           { return 0 }
+func (ArmourTierGold) KnockBackResistance() float64 { return 0 }
+func (ArmourTierGold) Name() string                 { return "golden" }
+
+// ArmourTierChain is the ArmourTier of chain armour.
+type ArmourTierChain struct{}
+
+func (ArmourTierChain) BaseDurability() float64      { return 166 }
+func (ArmourTierChain) Toughness() float64           { return 0 }
+func (ArmourTierChain) KnockBackResistance() float64 { return 0 }
+func (ArmourTierChain) Name() string                 { return "chainmail" }
+
+// ArmourTierIron is the ArmourTier of iron armour.
+type ArmourTierIron struct{}
+
+func (ArmourTierIron) BaseDurability() float64      { return 165 }
+func (ArmourTierIron) Toughness() float64           { return 0 }
+func (ArmourTierIron) KnockBackResistance() float64 { return 0 }
+func (ArmourTierIron) Name() string                 { return "iron" }
+
+// ArmourTierDiamond is the ArmourTier of diamond armour.
+type ArmourTierDiamond struct{}
+
+func (ArmourTierDiamond) BaseDurability() float64      { return 363 }
+func (ArmourTierDiamond) Toughness() float64           { return 2 }
+func (ArmourTierDiamond) KnockBackResistance() float64 { return 0 }
+func (ArmourTierDiamond) Name() string                 { return "diamond" }
+
+// ArmourTierNetherite is the ArmourTier of netherite armour.
+type ArmourTierNetherite struct{}
+
+func (ArmourTierNetherite) BaseDurability() float64      { return 408 }
+func (ArmourTierNetherite) Toughness() float64           { return 3 }
+func (ArmourTierNetherite) KnockBackResistance() float64 { return 0.1 }
+func (ArmourTierNetherite) Name() string                 { return "netherite" }
+
 // ArmourTiers returns a list of all armour tiers.
 func ArmourTiers() []ArmourTier {
-	return []ArmourTier{ArmourTierLeather, ArmourTierGold, ArmourTierChain, ArmourTierIron, ArmourTierDiamond, ArmourTierNetherite}
+	return []ArmourTier{ArmourTierLeather{}, ArmourTierGold{}, ArmourTierChain{}, ArmourTierIron{}, ArmourTierDiamond{}, ArmourTierNetherite{}}
 }
 
 // armourTierRepairable returns true if the ArmourTier passed is repairable.
 func armourTierRepairable(tier ArmourTier) func(Stack) bool {
 	return func(stack Stack) bool {
 		var ok bool
-		switch tier {
+		switch tier.(type) {
 		case ArmourTierLeather:
 			_, ok = stack.Item().(Leather)
 		case ArmourTierGold:
