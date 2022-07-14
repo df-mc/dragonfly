7a8c2bdf5e1e579d33cf031bd50b3108f11b842a
JustTal <justtal@protonmail.com>
2022-07-13 20:30:21 -0500

    main.go: Removed debugging code.

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/df-mc/dragonfly/server"
-	"github.com/df-mc/dragonfly/server/item"
-	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/player/chat"
-	"github.com/df-mc/dragonfly/server/world"
 	"github.com/pelletier/go-toml"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
@@ -31,15 +28,7 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	for srv.Accept(func(p *player.Player) {
-		inv := p.Inventory()
-		inv.Clear()
-		inv.AddItem(item.NewStack(item.Book{}, 64))
-		inv.AddItem(item.NewStack(item.LapisLazuli{}, 64))
-
-		p.SetGameMode(world.GameModeSurvival)
-		p.AddExperience(100000)
-	}) {
+	for srv.Accept(nil) {
 	}
 }
 
