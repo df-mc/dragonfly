294f6739d1097832c0fcb0016347f26600c0e59f
T14Raptor <t14raptor@gmail.com>
2022-07-15 16:42:26 -0400

    block/break_info.go: Correct doc.

--- server/block/break_info.go
+++ server/block/break_info.go
@@ -91,7 +91,7 @@ type BreakInfo struct {
 }
 
 // newBreakInfo creates a BreakInfo struct with the properties passed. The XPDrops field is 0 by default. The blast
-// resistance gets set to the hardness multiplied by five by default, as it usually is the same.
+// resistance is set to the hardness by default, as it usually is the same.
 func newBreakInfo(hardness float64, harvestable func(item.Tool) bool, effective func(item.Tool) bool, drops func(item.Tool, []item.Enchantment) []item.Stack) BreakInfo {
 	return BreakInfo{
 		Hardness:        hardness,
