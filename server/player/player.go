525ac6398f02b73fc6cbb66326ddda636e8c2509
JustTal <justtal@protonmail.com>
2022-07-16 15:29:25 -0500

    player/player.go: Possibly fix protection?

--- server/player/player.go
+++ server/player/player.go
@@ -596,26 +596,28 @@ func (p *Player) FinalDamageFrom(dmg float64, src damage.Source) float64 {
 				toughness += a.Toughness()
 			}
 		}
+
+		// First calculate the enchantment protection factor and cap it at 25. Then, multiply it by a random percent
+		// and round up.
+		f := p.enchantmentProtectionFactor(src)
+		if f > 25 {
+			f = 25
+		}
+		m := math.Ceil(float64(f) * (float64(rand.Intn(100-50)+50) / 100.0))
+		if m > 20 {
+			m = 20
+		}
+
 		// Armour in Bedrock edition reduces the damage taken by 4% for each effective armour point. Effective
 		// armour point decreases as damage increases, with 1 point lost for every 2 HP of damage. The defense
 		// reduction is decreased by the toughness armor value. Effective armour points will at minimum be 20% of
 		// armour points.
-		dmg -= dmg * 0.04 * math.Max(defencePoints*0.2, defencePoints-dmg/(2+toughness/4))
+		dmg -= dmg * m * 0.04 * math.Max(defencePoints*0.2, defencePoints-dmg/(2+toughness/4))
 	}
 	if res, ok := p.Effect(effect.Resistance{}); ok {
 		dmg *= effect.Resistance{}.Multiplier(src, res.Level())
 	}
 
-	f := p.enchantmentProtectionFactor(src)
-	if f > 25 {
-		f = 25
-	}
-	m := math.Ceil(float64(f) * (float64(rand.Intn(100-50)+50) / 100.0))
-	if m > 20 {
-		m = 20
-	}
-
-	dmg -= -dmg * m * 0.04
 	return math.Max(dmg, 0)
 }
 
