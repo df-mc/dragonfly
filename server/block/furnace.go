a48147e23a6ec98b40a477b4dec77cfac328849f
Tal <justtal@protonmail.com>
2022-07-17 04:12:14 -0500

    block/furnace.go: Drop items in inventory.

--- server/block/furnace.go
+++ server/block/furnace.go
@@ -72,7 +72,7 @@ func (f Furnace) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *world
 // BreakInfo ...
 func (f Furnace) BreakInfo() BreakInfo {
 	xp := f.Experience()
-	return newBreakInfo(3.5, pickaxeHarvestable, pickaxeEffective, oneOf(f)).withXPDropRange(xp, xp)
+	return newBreakInfo(3.5, pickaxeHarvestable, pickaxeEffective, simpleDrops(append(c.inventory.Items(), item.NewStack(f, 1))...)).withXPDropRange(xp, xp)
 }
 
 // Activate ...
