b3aa6e359ffd904713b6e8a9afcf82eb7d3e3c0b
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-07-22 02:14:53 +0800

    Tracked objects

--- server/session/handler_map_info_request.go
+++ server/session/handler_map_info_request.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/item/inventory"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
 
@@ -44,23 +45,37 @@ func (h *MapInfoRequestHandler) Handle(p packet.Packet, s *Session) error {
 		pixels = data.Pixels
 		height = int32(len(data.Pixels))
 		width  int32
+
+		trackeds []protocol.MapTrackedObject
 	)
 	for _, rows := range data.Pixels {
 		if len(rows) > int(width) {
 			width = int32(len(rows))
 		}
 	}
+	for _, e := range data.TrackEntities {
+		trackeds = append(trackeds, protocol.MapTrackedObject{
+			Type:           protocol.MapObjectTypeEntity,
+			EntityUniqueID: int64(s.entityRuntimeID(e)),
+		})
+	}
+	for _, p := range data.TrackBlocks {
+		trackeds = append(trackeds, protocol.MapTrackedObject{
+			Type:          protocol.MapObjectTypeBlock,
+			BlockPosition: protocol.BlockPos{int32(p[0]), int32(p[1]), int32(p[2])},
+		})
+	}
 
 	s.writePacket(&packet.ClientBoundMapItemData{
-		MapID:       pk.MapID,
-		UpdateFlags: packet.MapUpdateFlagInitialisation,
-		Dimension:   byte(mapItem.GetDimension().EncodeDimension()),
-		LockedMap:   false, // TODO: Locked map support
-		Scale:       byte(mapItem.GetScale()),
-		// TrackedObjects []protocol.MapTrackedObject
+		MapID:          pk.MapID,
+		UpdateFlags:    packet.MapUpdateFlagInitialisation,
+		Dimension:      byte(mapItem.GetDimension().EncodeDimension()),
+		LockedMap:      false, // TODO: Locked map support
+		Scale:          byte(mapItem.GetScale()),
+		TrackedObjects: trackeds,
 		// Decorations is a list of fixed decorations located on the map. The decorations will not change
 		// client-side, unless the server updates them.
-		// Decorations []protocol.MapDecoration
+		Decorations: []protocol.MapDecoration{},
 
 		Height: height,
 		Width:  width,
