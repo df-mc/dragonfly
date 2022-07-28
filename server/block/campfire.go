e90dc8f9028d20a583018eee71b009657dc92679
abimekuriya <abi.mek2005@gmail.com>
2022-07-28 19:20:58 -0400

    Remove Flint and Steel ignition behaviour from Activate()

--- server/block/campfire.go
+++ server/block/campfire.go
@@ -7,6 +7,7 @@ import (
 	"github.com/df-mc/dragonfly/server/entity/damage"
 	"github.com/df-mc/dragonfly/server/internal/nbtconv"
 	"github.com/df-mc/dragonfly/server/item"
+	"github.com/df-mc/dragonfly/server/item/enchantment"
 	"github.com/df-mc/dragonfly/server/item/potion"
 	"github.com/df-mc/dragonfly/server/world"
 	"github.com/df-mc/dragonfly/server/world/sound"
@@ -97,13 +98,22 @@ func (c Campfire) LightEmissionLevel() uint8 {
 	return c.Type.LightLevel()
 }
 
+// Ignite ...
+func (c Campfire) Ignite(pos cube.Pos, w *world.World) bool {
+	w.PlaySound(pos.Vec3(), sound.Ignite{})
+	if _, ok := w.Liquid(pos); ok {
+		return false
+	}
+	c.Extinguished = false
+	w.SetBlock(pos, c, nil)
+	return true
+}
+
 // Activate ...
 func (c Campfire) Activate(pos cube.Pos, _ cube.Face, w *world.World, u item.User, ctx *item.UseContext) bool {
 	if held, _ := u.HeldItems(); !held.Empty() {
-		if _, ok := held.Item().(item.FlintAndSteel); ok {
-			w.PlaySound(pos.Vec3(), sound.Ignite{})
-			c.Extinguished = false
-			w.SetBlock(pos, c, nil)
+		if _, ok := held.Enchantment(enchantment.FireAspect{}); ok {
+			c.Ignite(pos, w)
 			return true
 		}
 		if rawFood, ok := held.Item().(item.Smeltable); ok && rawFood.SmeltInfo().Food {
