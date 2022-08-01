8475ae5a4240d4013b17a1d4574941ec5e206840
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2022-08-01 11:13:53 -0500

    Added just fletching tables. Sorry twisted

---
+++ server/block/fletching_table.go
@@ -0,0 +1,37 @@
+package block
+
+import (
+	"github.com/df-mc/dragonfly/server/item"
+	"time"
+)
+
+// Fletching Table
+type FletchingTable struct {
+	solid
+	bassDrum
+}
+
+// BreakInfo ...
+func (f FletchingTable) BreakInfo() BreakInfo {
+	return newBreakInfo(2.5, alwaysHarvestable, axeEffective, silkTouchOnlyDrop(f))
+}
+
+// FlammabilityInfo ...
+func (FletchingTable) FlammabilityInfo() FlammabilityInfo {
+	return newFlammabilityInfo(5, 5, true)
+}
+
+// FuelInfo ...
+func (FletchingTable) FuelInfo() item.FuelInfo {
+	return newFuelInfo(time.Second * 15)
+}
+
+// EncodeItem ...
+func (FletchingTable) EncodeItem() (name string, meta int16) {
+	return "minecraft:fletching_table", 0
+}
+
+// EncodeBlock ...
+func (FletchingTable) EncodeBlock() (name string, properties map[string]any) {
+	return "minecraft:fletching_table", nil
+}
