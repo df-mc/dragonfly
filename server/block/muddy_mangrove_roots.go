474ddefa60d81c560a737a86c175f8e83cd40109
Tal <justtal@protonmail.com>
2022-08-09 10:48:11 -0500

    dragonfly: Initial support for v1.19.20 (#628)

--- server/block/muddy_mangrove_roots.go
+++ server/block/muddy_mangrove_roots.go
@@ -1,10 +1,23 @@
 package block
 
-import "github.com/df-mc/dragonfly/server/world"
+import (
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/item"
+	"github.com/df-mc/dragonfly/server/world"
+	"github.com/go-gl/mathgl/mgl64"
+)
 
 // MuddyMangroveRoots are a decorative variant of mangrove roots.
 type MuddyMangroveRoots struct {
 	solid
+
+	// Axis is the axis which the basalt faces.
+	Axis cube.Axis
+}
+
+// BreakInfo ...
+func (m MuddyMangroveRoots) BreakInfo() BreakInfo {
+	return newBreakInfo(0.7, alwaysHarvestable, shovelEffective, oneOf(m))
 }
 
 // SoilFor ...
@@ -16,9 +29,16 @@ func (MuddyMangroveRoots) SoilFor(block world.Block) bool {
 	return false
 }
 
-// BreakInfo ...
-func (m MuddyMangroveRoots) BreakInfo() BreakInfo {
-	return newBreakInfo(0.7, alwaysHarvestable, shovelEffective, oneOf(m))
+// UseOnBlock ...
+func (m MuddyMangroveRoots) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *world.World, user item.User, ctx *item.UseContext) (used bool) {
+	pos, face, used = firstReplaceable(w, pos, face, m)
+	if !used {
+		return
+	}
+	m.Axis = face.Axis()
+
+	place(w, pos, m, user, ctx)
+	return placed(ctx)
 }
 
 // EncodeItem ...
@@ -27,6 +47,14 @@ func (MuddyMangroveRoots) EncodeItem() (name string, meta int16) {
 }
 
 // EncodeBlock ...
-func (MuddyMangroveRoots) EncodeBlock() (string, map[string]any) {
-	return "minecraft:muddy_mangrove_roots", nil
+func (m MuddyMangroveRoots) EncodeBlock() (string, map[string]any) {
+	return "minecraft:muddy_mangrove_roots", map[string]any{"pillar_axis": m.Axis.String()}
+}
+
+// allMuddyMangroveRoots ...
+func allMuddyMangroveRoots() (roots []world.Block) {
+	for _, axis := range cube.Axes() {
+		roots = append(roots, MuddyMangroveRoots{Axis: axis})
+	}
+	return
 }
