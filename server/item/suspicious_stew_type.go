7ee0608135e050602b8999b49399d19ab96b0eb8
Emin Arıcan <emin.arican@outlook.com>
2022-08-31 03:37:05 +0300

    remove empty lines

--- server/item/suspicious_stew_type.go
+++ server/item/suspicious_stew_type.go
@@ -15,68 +15,56 @@ type StewType struct {
 // NightVisionStew returns suspicious stew night vision effect.
 func NightVisionStew() StewType {
 	return StewType{0}
-
 }
 
 // JumpBoostStew returns suspicious stew jump boost effect.
 func JumpBoostStew() StewType {
 	return StewType{1}
-
 }
 
 // WeaknessStew returns suspicious stew weakness effect.
 func WeaknessStew() StewType {
 	return StewType{2}
-
 }
 
 // BlindnessStew returns suspicious stew blindness effect.
-
 func BlindnessStew() StewType {
 	return StewType{3}
-
 }
 
 // PoisonStew returns suspicious stew poison effect.
 func PoisonStew() StewType {
 	return StewType{4}
-
 }
 
 // SaturationDandelionStew returns suspicious stew saturation effect.
 func SaturationDandelionStew() StewType {
 	return StewType{5}
-
 }
 
 // SaturationOrchidStew returns suspicious stew saturation effect.
 func SaturationOrchidStew() StewType {
 	return StewType{6}
-
 }
 
 // FireResistanceStew returns suspicious stew fire resistance effect.
 func FireResistanceStew() StewType {
 	return StewType{7}
-
 }
 
 // RegenerationStew returns suspicious stew regeneration effect.
 func RegenerationStew() StewType {
 	return StewType{8}
-
 }
 
 // WitherStew returns suspicious stew wither effect.
 func WitherStew() StewType {
 	return StewType{9}
-
 }
 
 // StewTypes ...
 func StewTypes() []StewType {
 	return []StewType{NightVisionStew(), JumpBoostStew(), WeaknessStew(), BlindnessStew(), PoisonStew(), SaturationDandelionStew(), SaturationOrchidStew(), FireResistanceStew(), RegenerationStew(), WitherStew()}
-
 }
 
 type stewType uint8
