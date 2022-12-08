53fac60acc866071a6323bbfe9809cd156ddcd0c
Sandertv <st.ten.veldhuis@gmail.com>
2022-12-08 21:53:50 +0100

    entity/ent.go: Initial concept for a shared entity type for entities. Note: This is very much not thread safe and might very well not work yet. It is only a concept for now.

---
+++ server/entity/ent.go
@@ -0,0 +1,110 @@
+package entity
+
+import (
+	"github.com/df-mc/dragonfly/server/block"
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/world"
+	"github.com/go-gl/mathgl/mgl64"
+	"sync"
+)
+
+type Lifetime interface {
+	Tick(e *Ent) *Movement
+
+	Explode(e *Ent, src mgl64.Vec3, impact float64, conf block.ExplosionConfig)
+}
+
+type Config struct {
+	Lifetime Lifetime
+}
+
+func (conf Config) New(t world.EntityType, pos mgl64.Vec3) *Ent {
+	return &Ent{t: t, pos: pos}
+}
+
+type Ent struct {
+	conf Config
+	t    world.EntityType
+
+	mu  sync.Mutex
+	pos mgl64.Vec3
+	vel mgl64.Vec3
+	rot cube.Rotation
+}
+
+func (e *Ent) Explode(src mgl64.Vec3, impact float64, conf block.ExplosionConfig) {
+	e.conf.Lifetime.Explode(e, src, impact, conf)
+}
+
+func NewEnt(t world.EntityType, pos mgl64.Vec3) *Ent {
+	var conf Config
+	// TODO: Default lifetime. What would the behaviour of that be?
+	return conf.New(t, pos)
+}
+
+// Type returns the world.EntityType passed to NewEnt.
+func (e *Ent) Type() world.EntityType {
+	return e.t
+}
+
+func (e *Ent) Owner() world.Entity {
+	// TODO: Change this signature to Owner() (world.Entity, bool) once all
+	//  entities use this type.
+	if owned, ok := e.conf.Lifetime.(interface {
+		Owner() world.Entity
+	}); ok {
+		return owned.Owner()
+	}
+	return nil
+}
+
+// Position returns the current position of the entity.
+func (e *Ent) Position() mgl64.Vec3 {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	return e.pos
+}
+
+// Velocity returns the current velocity of the entity. The values in the Vec3 returned represent the speed on
+// that axis in blocks/tick.
+func (e *Ent) Velocity() mgl64.Vec3 {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	return e.vel
+}
+
+// SetVelocity sets the velocity of the entity. The values in the Vec3 passed represent the speed on
+// that axis in blocks/tick.
+func (e *Ent) SetVelocity(v mgl64.Vec3) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	e.vel = v
+}
+
+// Rotation always returns an empty cube.Rotation.
+func (e *Ent) Rotation() cube.Rotation {
+	return e.rot
+}
+
+// World returns the world of the entity.
+func (e *Ent) World() *world.World {
+	w, _ := world.OfEntity(e)
+	return w
+}
+
+// Tick ticks Ent, progressing its lifetime and closing the entity if it is
+// in the void.
+func (e *Ent) Tick(w *world.World, current int64) {
+	if e.pos[1] < float64(w.Range()[0]) && current%10 == 0 {
+		_ = e.Close()
+		return
+	}
+	m := e.conf.Lifetime.Tick(e)
+	m.Send()
+}
+
+// Close closes the Ent and removes the associated entity from the world.
+func (e *Ent) Close() error {
+	e.World().RemoveEntity(e)
+	return nil
+}
--- server/entity/projectile.go
+++ server/entity/projectile.go
@@ -1,11 +1,111 @@
 package entity
 
 import (
+	"github.com/df-mc/dragonfly/server/block"
+	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/block/cube/trace"
 	"github.com/df-mc/dragonfly/server/world"
 	"github.com/go-gl/mathgl/mgl64"
 	"math"
 )
+
+type ProjectileLifetimeConfig struct {
+	Owner world.Entity
+
+	Gravity float64
+
+	Drag float64
+
+	Damage float64
+
+	Particle world.Particle
+}
+
+func (conf ProjectileLifetimeConfig) New() *ProjectileLifetime {
+	return &ProjectileLifetime{conf: conf, mc: &MovementComputer{
+		Gravity:           conf.Gravity,
+		Drag:              conf.Drag,
+		DragBeforeGravity: true,
+	}}
+}
+
+type ProjectileLifetime struct {
+	conf  ProjectileLifetimeConfig
+	mc    *MovementComputer
+	age   int
+	close bool
+}
+
+func (lt *ProjectileLifetime) Explode(e *Ent, src mgl64.Vec3, impact float64, _ block.ExplosionConfig) {
+	e.vel = e.vel.Add(e.pos.Sub(src).Normalize().Mul(impact))
+}
+
+func (lt *ProjectileLifetime) Tick(e *Ent) *Movement {
+	if lt.close {
+		_ = e.Close()
+		return nil
+	}
+	m, result := lt.tickMovement(e)
+	if result == nil {
+		return m
+	}
+
+	if lt.conf.Particle != nil {
+		for i := 0; i < 6; i++ {
+			e.World().AddParticle(result.Position(), lt.conf.Particle)
+		}
+	}
+
+	if r, ok := result.(trace.EntityResult); ok {
+		if l, ok := r.Entity().(Living); ok {
+			src := ProjectileDamageSource{Projectile: e, Owner: lt.conf.Owner}
+			if _, vulnerable := l.Hurt(lt.conf.Damage, src); vulnerable {
+				l.KnockBack(m.pos, 0.45, 0.3608)
+			}
+		}
+	}
+
+	lt.close = true
+	return m
+}
+
+func (lt *ProjectileLifetime) tickMovement(e *Ent) (*Movement, trace.Result) {
+	w, pos, vel, rot := e.World(), e.Position(), e.Velocity(), e.Rotation()
+	viewers := w.Viewers(pos)
+
+	velBefore := vel
+	vel = lt.mc.applyHorizontalForces(w, pos, lt.mc.applyVerticalForces(vel))
+	rot = cube.Rotation{
+		mgl64.RadToDeg(math.Atan2(vel[0], vel[2])),
+		mgl64.RadToDeg(math.Atan2(vel[1], math.Hypot(vel[0], vel[2]))),
+	}
+
+	var (
+		end = pos.Add(vel)
+		hit trace.Result
+		ok  bool
+	)
+	if !mgl64.FloatEqual(end.Sub(pos).LenSqr(), 0) {
+		if hit, ok = trace.Perform(pos, end, w, e.Type().BBox(e).Grow(1.0), lt.ignores(e)); ok {
+			vel = zeroVec3
+			end = hit.Position()
+		}
+	}
+	lt.age++
+
+	return &Movement{v: viewers, e: e, pos: end, vel: vel, dpos: end.Sub(pos), dvel: vel.Sub(velBefore), rot: rot}, hit
+}
+
+// ignores returns a function to ignore entities in trace.Perform that are
+// either a spectator, not living, the entity itself or its owner in the first
+// 5 ticks.
+func (lt *ProjectileLifetime) ignores(e *Ent) func(other world.Entity) bool {
+	return func(other world.Entity) bool {
+		g, ok := other.(interface{ GameMode() world.GameMode })
+		_, living := other.(Living)
+		return (ok && !g.GameMode().HasCollision()) || e == other || !living || (lt.age < 5 && lt.conf.Owner == other)
+	}
+}
 
 // Projectile is a world.Entity that can be fired by another entity. It has an
 // Owner method.
@@ -61,6 +161,6 @@ func (c *ProjectileComputer) TickMovement(e Projectile, pos, vel mgl64.Vec3, yaw
 
 	return &Movement{v: viewers, e: e,
 		pos: end, vel: vel, dpos: end.Sub(pos), dvel: vel.Sub(velBefore),
-		yaw: yaw, pitch: pitch, onGround: c.onGround,
+		rot: cube.Rotation{yaw, pitch}, onGround: c.onGround,
 	}, hit
 }
