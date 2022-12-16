0801cb76b3b12bafae18c93f262dd510e75f4a8a
Tal <justtal@protonmail.com>
2022-12-16 10:04:52 -0600

    world/world.go: Avoid saving unmodified chunks (#705)

--- server/world/world.go
+++ server/world/world.go
@@ -245,6 +245,7 @@ func (w *World) SetBlock(pos cube.Pos, b Block, opts *SetOpts) {
 		before = c.Block(x, y, z, 0)
 	}
 
+	c.m = true
 	c.SetBlock(x, y, z, 0, rid)
 	if nbtBlocks[rid] {
 		c.e[pos] = b
@@ -300,6 +301,8 @@ func (w *World) SetBiome(pos cube.Pos, b Biome) {
 	}
 	c := w.chunk(chunkPosFromBlockPos(pos))
 	defer c.Unlock()
+
+	c.m = true
 	c.SetBiome(uint8(pos[0]), int16(pos[1]), uint8(pos[2]), uint32(b.EncodeBiome()))
 }
 
@@ -383,6 +386,7 @@ func (w *World) BuildStructure(pos cube.Pos, s Structure) {
 				}
 			}
 			c.SetBlock(0, 0, 0, 0, c.Block(0, 0, 0, 0)) // Make sure the heightmap is recalculated.
+			c.m = true
 
 			// After setting all blocks of the structure within a single chunk, we show the new chunk to all
 			// viewers once, and unlock it.
@@ -462,6 +466,7 @@ func (w *World) SetLiquid(pos cube.Pos, b Liquid) {
 			v.ViewBlockUpdate(pos, b, 1)
 		}
 	}
+	c.m = true
 	c.Unlock()
 
 	w.doBlockUpdatesAround(pos)
@@ -1330,21 +1335,26 @@ func (w *World) loadIntoBlocks(c *chunkData, blockEntityData []map[string]any) {
 // the provider.
 func (w *World) saveChunk(pos ChunkPos, c *chunkData) {
 	c.Lock()
-	// We allocate a new map for all block entities.
-	m := make([]map[string]any, 0, len(c.e))
-	for pos, b := range c.e {
-		if n, ok := b.(NBTer); ok {
-			// Encode the block entities and add the 'x', 'y' and 'z' tags to it.
-			data := n.EncodeNBT()
-			data["x"], data["y"], data["z"] = int32(pos[0]), int32(pos[1]), int32(pos[2])
-			m = append(m, data)
-		}
-	}
 	if !w.conf.ReadOnly {
-		c.Compact()
-		if err := w.provider().SaveChunk(pos, c.Chunk, w.conf.Dim); err != nil {
-			w.conf.Log.Errorf("error saving chunk %v to provider: %v", pos, err)
+		if len(c.e) > 0 || c.m {
+			c.Compact()
+			if err := w.provider().SaveChunk(pos, c.Chunk, w.conf.Dim); err != nil {
+				w.conf.Log.Errorf("error saving chunk %v to provider: %v", pos, err)
+			}
+
+			m := make([]map[string]any, 0, len(c.e))
+			for pos, b := range c.e {
+				if n, ok := b.(NBTer); ok {
+					data := n.EncodeNBT()
+					data["x"], data["y"], data["z"] = int32(pos[0]), int32(pos[1]), int32(pos[2])
+					m = append(m, data)
+				}
+			}
+			if err := w.provider().SaveBlockNBT(pos, m, w.conf.Dim); err != nil {
+				w.conf.Log.Errorf("error saving block NBT in chunk %v to provider: %v", pos, err)
+			}
 		}
+
 		s := make([]Entity, 0, len(c.entities))
 		for _, e := range c.entities {
 			if _, ok := e.Type().(SaveableEntityType); ok {
@@ -1353,9 +1363,6 @@ func (w *World) saveChunk(pos ChunkPos, c *chunkData) {
 		}
 		if err := w.provider().SaveEntities(pos, s, w.conf.Dim); err != nil {
 			w.conf.Log.Errorf("error saving entities in chunk %v to provider: %v", pos, err)
-		}
-		if err := w.provider().SaveBlockNBT(pos, m, w.conf.Dim); err != nil {
-			w.conf.Log.Errorf("error saving block NBT in chunk %v to provider: %v", pos, err)
 		}
 	}
 	ent := c.entities
@@ -1407,6 +1414,7 @@ func (w *World) chunkCacheJanitor() {
 // by the mutex present in the chunk.Chunk held.
 type chunkData struct {
 	*chunk.Chunk
+	m        bool
 	e        map[cube.Pos]Block
 	v        []Viewer
 	l        []*Loader
