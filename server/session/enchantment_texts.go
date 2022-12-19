b5157336394b44f20dea7dbc43e5e1b4ee5d738f
JustTalDevelops <JustTalDevelops@users.noreply.github.com>
2022-12-19 16:22:11 +0000

    updated contributor list

--- server/session/enchantment_texts.go
+++ server/session/enchantment_texts.go
@@ -4,4 +4,4 @@ package session
 
 // enchantNames are names translated to the 'Standard Galactic Alphabet' client-side. The names generally have no meaning
 // on the vanilla server implementation, so we can sneak some easter eggs in here without anyone noticing.
-var enchantNames = []string{"abimek", "aericio", "aimjel", "alvin0319", "andreas hgk", "atm85", "blackjack200", "da pig guy", "deniel world", "eminarican", "endermanbugzjfc", "flonja", "hochbaum", "hyper flare mc", "im da real ani", "its zodia x", "just tal develops", "liatoast", "mmm545", "mohamed587100", "neutronic mc", "nonono697", "prim69", "provsalt", "restart fu", "robertdudaa", "royal mcpe", "sa111n111", "sallypemdas", "sandertv", "sculas", "t14 raptor", "tadhunt", "thunder33345", "tristanmorgan", "twisted asylum mc", "unickorn", "unknown ore", "uramnoil", "varun-balani", "wqrro", "xd-pro"}
+var enchantNames = []string{}
