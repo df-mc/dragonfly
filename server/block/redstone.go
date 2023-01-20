c039fd268371a55e46fffc3976a25da12802e029
JustTal <justtal@protonmail.com>
2023-01-20 16:14:08 -0600

    block/redstone.go: i think this is it?

--- server/block/redstone.go
+++ server/block/redstone.go
@@ -97,12 +97,7 @@ func updateDirectionalRedstone(pos cube.Pos, w *world.World, face cube.Face) {
 
 // updateGateRedstone is used to update redstone gates on each face of the given offset centre position.
 func updateGateRedstone(centre cube.Pos, w *world.World, face cube.Face) {
-	pos := centre.Side(face)
-	if r, ok := w.Block(pos).(RedstoneUpdater); ok {
-		r.RedstoneUpdate(pos, w)
-	}
-
-	updateAroundRedstone(pos, w, face.Opposite())
+	updateAroundRedstone(centre.Side(face), w, face.Opposite())
 }
 
 // identifyNeighbours identifies the neighbouring positions of a given node, determines their types, and links them into
