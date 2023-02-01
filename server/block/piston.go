d6c3189ba4a68d3fff90e9c54df2fc60e4863aee
JustTal <justtal@protonmail.com>
2023-01-31 21:09:48 -0600

    block/piston.go: pulling but it's still really broken lmao

--- server/block/piston.go
+++ server/block/piston.go
@@ -20,11 +20,9 @@ type Piston struct {
 	Sticky bool
 
 	// AttachedBlocks ...
-	// TODO: Make this a []cube.Pos and convert to []int32 on encode.
-	AttachedBlocks []int32
+	AttachedBlocks []cube.Pos
 	// BreakBlocks ...
-	// TODO: Make this a []cube.Pos and convert to []int32 on encode.
-	BreakBlocks []int32
+	BreakBlocks []cube.Pos
 
 	// Progress is how far the block has been moved. It can either be 0.0, 0.5, or 1.0.
 	Progress float64
@@ -72,9 +70,17 @@ func (p Piston) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *world.
 
 // EncodeNBT ...
 func (p Piston) EncodeNBT() map[string]any {
+	attachedBlocks := make([]int32, 0, len(p.AttachedBlocks)*3)
+	for _, pos := range p.AttachedBlocks {
+		attachedBlocks = append(attachedBlocks, int32(pos[0]), int32(pos[1]), int32(pos[2]))
+	}
+	breakBlocks := make([]int32, 0, len(p.BreakBlocks)*3)
+	for _, pos := range p.BreakBlocks {
+		breakBlocks = append(breakBlocks, int32(pos[0]), int32(pos[1]), int32(pos[2]))
+	}
 	return map[string]any{
-		"AttachedBlocks": p.AttachedBlocks,
-		"BreakBlocks":    p.BreakBlocks,
+		"AttachedBlocks": attachedBlocks,
+		"BreakBlocks":    breakBlocks,
 
 		"LastProgress": float32(p.LastProgress),
 		"Progress":     float32(p.Progress),
@@ -91,15 +97,23 @@ func (p Piston) EncodeNBT() map[string]any {
 // DecodeNBT ...
 func (p Piston) DecodeNBT(m map[string]any) any {
 	if attached := nbtconv.Slice(m, "AttachedBlocks"); attached != nil {
-		p.AttachedBlocks = make([]int32, len(attached))
-		for i, v := range attached {
-			p.AttachedBlocks[i] = v.(int32)
+		p.AttachedBlocks = make([]cube.Pos, 0, len(attached)/3)
+		for i := 0; i < len(attached); i += 3 {
+			p.AttachedBlocks = append(p.AttachedBlocks, cube.Pos{
+				int(attached[i].(int32)),
+				int(attached[i+1].(int32)),
+				int(attached[i+2].(int32)),
+			})
 		}
 	}
 	if breakBlocks := nbtconv.Slice(m, "BreakBlocks"); breakBlocks != nil {
-		p.BreakBlocks = make([]int32, len(breakBlocks))
-		for i, v := range breakBlocks {
-			p.BreakBlocks[i] = v.(int32)
+		p.BreakBlocks = make([]cube.Pos, 0, len(breakBlocks)/3)
+		for i := 0; i < len(breakBlocks); i += 3 {
+			p.BreakBlocks = append(p.BreakBlocks, cube.Pos{
+				int(breakBlocks[i].(int32)),
+				int(breakBlocks[i+1].(int32)),
+				int(breakBlocks[i+2].(int32)),
+			})
 		}
 	}
 	p.LastProgress = float64(nbtconv.Float32(m, "LastProgress"))
@@ -144,7 +158,7 @@ func (p Piston) push(pos cube.Pos, w *world.World) bool {
 		}
 
 		for _, breakPos := range resolver.breakPositions {
-			p.BreakBlocks = append(p.BreakBlocks, int32(breakPos.X()), int32(breakPos.Y()), int32(breakPos.Z()))
+			p.BreakBlocks = append(p.BreakBlocks, breakPos)
 			if b, ok := w.Block(breakPos).(Breakable); ok {
 				w.SetBlock(breakPos, nil, nil)
 				for _, drop := range b.BreakInfo().Drops(item.ToolNone{}, nil) {
@@ -156,7 +170,7 @@ func (p Piston) push(pos cube.Pos, w *world.World) bool {
 		face := p.armFace()
 		for _, attachedPos := range resolver.attachedPositions {
 			side := attachedPos.Side(face)
-			p.AttachedBlocks = append(p.AttachedBlocks, int32(side.X()), int32(side.Y()), int32(side.Z()))
+			p.AttachedBlocks = append(p.AttachedBlocks, side)
 
 			w.SetBlock(side, Moving{Piston: pos, Moving: w.Block(attachedPos)}, nil)
 			w.SetBlock(attachedPos, nil, nil)
@@ -174,32 +188,27 @@ func (p Piston) push(pos cube.Pos, w *world.World) bool {
 		if p.State == 1 {
 			p.Progress += 0.5
 			if p.Progress == 0.5 {
-				// TODO: Sound!
+				// TODO: Out sound!
 			}
 		}
 
 		if p.State == 2 {
-			for i := 0; i < len(p.AttachedBlocks); i += 3 {
-				x := p.AttachedBlocks[i]
-				y := p.AttachedBlocks[i+1]
-				z := p.AttachedBlocks[i+2]
-
-				attachPos := cube.Pos{int(x), int(y), int(z)}
-				moving, ok := w.Block(attachPos).(Moving)
+			for _, attachedPos := range p.AttachedBlocks {
+				moving, ok := w.Block(attachedPos).(Moving)
 				if !ok {
 					continue
 				}
-				w.SetBlock(attachPos, moving.Moving, nil)
+
+				w.SetBlock(attachedPos, moving.Moving, nil)
 				if r, ok := moving.Moving.(RedstoneUpdater); ok {
-					r.RedstoneUpdate(attachPos, w)
+					r.RedstoneUpdate(attachedPos, w)
 				}
-				updateAroundRedstone(attachPos, w)
+				updateAroundRedstone(attachedPos, w)
 			}
 
 			p.AttachedBlocks = nil
 			p.BreakBlocks = nil
 		}
-		return false
 	} else if p.State == 3 {
 		return p.pull(pos, w)
 	} else {
@@ -213,8 +222,75 @@ func (p Piston) push(pos cube.Pos, w *world.World) bool {
 
 // pull ...
 func (p Piston) pull(pos cube.Pos, w *world.World) bool {
-	//TODO: Implement.
-	return false
+	if p.State == 2 {
+		face := p.armFace()
+		w.SetBlock(pos.Side(face), nil, nil)
+
+		resolver := pistonResolve(w, pos, p, false)
+		if !resolver.success {
+			return false
+		}
+
+		for _, breakPos := range resolver.breakPositions {
+			p.BreakBlocks = append(p.BreakBlocks, breakPos)
+			if b, ok := w.Block(breakPos).(Breakable); ok {
+				w.SetBlock(breakPos, nil, nil)
+				for _, drop := range b.BreakInfo().Drops(item.ToolNone{}, nil) {
+					dropItem(w, drop, breakPos.Vec3Centre())
+				}
+			}
+		}
+
+		face = face.Opposite()
+		for _, attachedPos := range resolver.attachedPositions {
+			side := attachedPos.Side(face)
+			p.AttachedBlocks = append(p.AttachedBlocks, side)
+
+			w.SetBlock(side, Moving{Piston: pos, Moving: w.Block(attachedPos)}, nil)
+			w.SetBlock(attachedPos, nil, nil)
+			updateAroundRedstone(attachedPos, w)
+		}
+
+		p.State = 3
+	} else if p.State == 3 {
+		if p.Progress == 0 {
+			p.State = 0
+		}
+		p.LastProgress = p.Progress
+
+		if p.State == 3 {
+			p.Progress -= 0.5
+			if p.Progress == 0.5 {
+				// TODO: In sound!
+			}
+		}
+
+		if p.State == 0 {
+			for _, attachedPos := range p.AttachedBlocks {
+				moving, ok := w.Block(attachedPos).(Moving)
+				if !ok {
+					continue
+				}
+
+				w.SetBlock(attachedPos, moving.Moving, nil)
+				if r, ok := moving.Moving.(RedstoneUpdater); ok {
+					r.RedstoneUpdate(attachedPos, w)
+				}
+				updateAroundRedstone(attachedPos, w)
+			}
+
+			p.AttachedBlocks = nil
+			p.BreakBlocks = nil
+		}
+	} else if p.State == 1 {
+		return p.push(pos, w)
+	} else {
+		return false
+	}
+
+	p.NewState = p.State
+	w.SetBlock(pos, p, nil)
+	return true
 }
 
 // allPistons ...
