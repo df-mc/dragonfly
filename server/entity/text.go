a7380da2fecdf1e5dd8cf54894419e63a3c8cbf8
Sandertv <st.ten.veldhuis@gmail.com>
2023-02-05 15:54:54 +0100

    entity/text.go: Moved to use Ent type and a new StationaryBehaviour.

--- server/entity/text.go
+++ server/entity/text.go
@@ -7,51 +7,14 @@ import (
 	"github.com/go-gl/mathgl/mgl64"
 )
 
-// Text is an entity that only displays floating text. The entity is otherwise invisible and cannot be moved.
-type Text struct {
-	transform
-	text string
-}
-
 // NewText creates and returns a new Text entity with the text and position provided.
-func NewText(text string, pos mgl64.Vec3) *Text {
-	t := &Text{text: text}
-	t.transform = newTransform(t, pos)
-	return t
+func NewText(text string, pos mgl64.Vec3) *Ent {
+	e := Config{Behaviour: textConf.New()}.New(TextType{}, pos)
+	e.SetNameTag(text)
+	return e
 }
 
-// Type returns TextType.
-func (*Text) Type() world.EntityType {
-	return TextType{}
-}
-
-// Immobile always returns true.
-func (t *Text) Immobile() bool {
-	return true
-}
-
-// SetText updates the designated text of the entity.
-func (t *Text) SetText(text string) {
-	t.mu.Lock()
-	t.text = text
-	t.mu.Unlock()
-
-	for _, v := range t.World().Viewers(t.Position()) {
-		v.ViewEntityState(t)
-	}
-}
-
-// Text returns the designated text of the entity.
-func (t *Text) Text() string {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	return t.text
-}
-
-// NameTag returns the designated text of the entity. It is an alias for the Text function.
-func (t *Text) NameTag() string {
-	return t.Text()
-}
+var textConf = StationaryBehaviourConfig{}
 
 // TextType is a world.EntityType implementation for Text.
 type TextType struct{}
@@ -65,9 +28,9 @@ func (TextType) DecodeNBT(m map[string]any) world.Entity {
 }
 
 func (TextType) EncodeNBT(e world.Entity) map[string]any {
-	t := e.(*Text)
+	t := e.(*Ent)
 	return map[string]any{
 		"Pos":  nbtconv.Vec3ToFloat32Slice(t.Position()),
-		"Text": t.text,
+		"Text": t.NameTag(),
 	}
 }
