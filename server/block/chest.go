53b3b21db193d37e99c876455f088bf8d8c1b9b4
Natsuri <86489758+xNatsuri@users.noreply.github.com>
2023-07-11 12:11:05 -0700

    block/chest.go: Allow chests to be opened if lava / water is above it. (#802)

--- server/block/chest.go
+++ server/block/chest.go
@@ -111,7 +111,7 @@ func (c Chest) RemoveViewer(v ContainerViewer, w *world.World, pos cube.Pos) {
 // Activate ...
 func (c Chest) Activate(pos cube.Pos, _ cube.Face, w *world.World, u item.User, _ *item.UseContext) bool {
 	if opener, ok := u.(ContainerOpener); ok {
-		if d, ok := w.Block(pos.Side(cube.FaceUp)).(LightDiffuser); ok && d.LightDiffusionLevel() == 0 {
+		if d, ok := w.Block(pos.Side(cube.FaceUp)).(LightDiffuser); ok && d.LightDiffusionLevel() <= 2 {
 			opener.OpenBlockContainer(pos)
 		}
 		return true
