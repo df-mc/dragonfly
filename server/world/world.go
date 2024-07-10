65d9481e67cf85c4815564ef7e73f539d52d2ecc
cqdetdisc <cqdetdisc@gmail.com>
2024-07-10 11:38:39 -0400

    fix entity deadlock

--- server/world/world.go
+++ server/world/world.go
@@ -118,25 +118,15 @@ func (w *World) Block(pos cube.Pos) Block {
 		return air()
 	}
 	c := w.chunk(chunkPosFromBlockPos(pos))
+	defer c.Unlock()
 
 	rid := c.Block(uint8(pos[0]), int16(pos[1]), uint8(pos[2]), 0)
 	if nbtBlocks[rid] {
 		// The block was also a block entity, so we look it up in the block entity map.
 		if nbtB, ok := c.BlockEntities[pos]; ok {
-			c.Unlock()
 			return nbtB
 		}
-		b, _ := BlockByRuntimeID(rid)
-		nbtB := b.(NBTer).DecodeNBT(map[string]any{}).(Block)
-		c.BlockEntities[pos] = nbtB
-		viewers := slices.Clone(c.viewers)
-		c.Unlock()
-		for _, v := range viewers {
-			v.ViewBlockUpdate(pos, nbtB, 0)
-		}
-		return nbtB
 	}
-	c.Unlock()
 	b, _ := BlockByRuntimeID(rid)
 	return b
 }
@@ -696,7 +686,6 @@ func (w *World) AddEntity(e Entity) {
 	c := w.chunk(chunkPos)
 	c.Entities = append(c.Entities, e)
 	viewers := slices.Clone(c.viewers)
-	c.modified = true
 	c.Unlock()
 
 	for _, v := range viewers {
@@ -745,7 +734,6 @@ func (w *World) RemoveEntity(e Entity) {
 	}
 	c.Entities = sliceutil.DeleteVal(c.Entities, e)
 	viewers := slices.Clone(c.viewers)
-	c.modified = true
 	c.Unlock()
 
 	w.entityMu.Lock()
@@ -950,7 +938,7 @@ func (w *World) ScheduleBlockUpdate(pos cube.Pos, delay time.Duration) {
 	t := w.set.CurrentTick
 	w.set.Unlock()
 
-	w.scheduledUpdates[pos] = t + delay.Nanoseconds()/int64(time.Second/20)
+	w.scheduledUpdates[pos] = t + (delay.Milliseconds() / 50)
 }
 
 // doBlockUpdatesAround schedules block updates directly around and on the position passed.
@@ -1018,6 +1006,33 @@ func (w *World) PortalDestination(dim Dimension) *World {
 	return w
 }
 
+// RedstonePower returns the level of redstone power being emitted from a position to the provided face.
+func (w *World) RedstonePower(pos cube.Pos, face cube.Face, accountForDust bool) (power int) {
+	b := w.Block(pos)
+	if c, ok := b.(Conductor); ok {
+		power = c.WeakPower(pos, face, w, accountForDust)
+	}
+	if b, ok := b.(redstoneBlocking); ok && b.RedstoneBlocking() {
+		return power
+	}
+	if d, ok := b.(lightDiffuser); ok && d.LightDiffusionLevel() == 0 {
+		return power
+	}
+	for _, f := range cube.Faces() {
+		if !b.Model().FaceSolid(pos, f, w) {
+			return power
+		}
+	}
+	for _, f := range cube.Faces() {
+		c, ok := w.Block(pos.Side(f)).(Conductor)
+		if !ok {
+			continue
+		}
+		power = max(power, c.StrongPower(pos.Side(f), f, w, accountForDust))
+	}
+	return power
+}
+
 // Close closes the world and saves all chunks currently loaded.
 func (w *World) Close() error {
 	if w == nil {
@@ -1045,7 +1060,7 @@ func (w *World) close() {
 	w.chunkMu.Unlock()
 
 	for pos, c := range toSave {
-		w.saveChunk(pos, c)
+		w.saveChunk(pos, c, true)
 	}
 
 	w.set.ref.Dec()
@@ -1062,6 +1077,19 @@ func (w *World) close() {
 	w.conf.Log.Debugf("Closing provider...")
 	if err := w.provider().Close(); err != nil {
 		w.conf.Log.Errorf("error closing world provider: %v", err)
+	}
+}
+
+// Save saves the World to the provider.
+func (w *World) Save() {
+	w.conf.Log.Debugf("Saving chunks in memory to disk...")
+
+	w.chunkMu.Lock()
+	toSave := maps.Clone(w.chunks)
+	w.chunkMu.Unlock()
+
+	for pos, c := range toSave {
+		w.saveChunk(pos, c, false)
 	}
 }
 
@@ -1315,20 +1343,24 @@ func (w *World) spreadLight(pos ChunkPos) {
 
 // saveChunk is called when a chunk is removed from the cache. We first compact the chunk, then we write it to
 // the provider.
-func (w *World) saveChunk(pos ChunkPos, c *Column) {
+func (w *World) saveChunk(pos ChunkPos, c *Column, closeEntities bool) {
 	c.Lock()
-	if !w.conf.ReadOnly && c.modified {
+	if !w.conf.ReadOnly && (len(c.BlockEntities) > 0 || len(c.Entities) > 0 || c.modified) {
 		c.Compact()
 		if err := w.provider().StoreColumn(pos, w.conf.Dim, c); err != nil {
 			w.conf.Log.Errorf("save chunk: %v", err)
 		}
 	}
-	ent := c.Entities
-	c.Entities = nil
-	c.Unlock()
+	if closeEntities {
+		ent := c.Entities
+		c.Entities = nil
+		c.Unlock()
 
-	for _, e := range ent {
-		_ = e.Close()
+		for _, e := range ent {
+			_ = e.Close()
+		}
+	} else {
+		c.Unlock()
 	}
 }
 
@@ -1358,7 +1390,7 @@ func (w *World) chunkCacheJanitor() {
 			w.chunkMu.Unlock()
 
 			for pos, c := range chunksToRemove {
-				w.saveChunk(pos, c)
+				w.saveChunk(pos, c, true)
 				delete(chunksToRemove, pos)
 			}
 		case <-w.closing:
@@ -1385,4 +1417,12 @@ type Column struct {
 // newColumn returns a new Column wrapper around the chunk.Chunk passed.
 func newColumn(c *chunk.Chunk) *Column {
 	return &Column{Chunk: c, BlockEntities: map[cube.Pos]Block{}}
+}
+
+// max returns the max of two integers.
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
