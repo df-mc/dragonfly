1e2bbf250a6e87454d869c0e53cea9cb8ddaf1b2
Jon <86489758+xNatsuri@users.noreply.github.com>
2024-08-18 01:08:12 -0700

    dragonfly/server: Implement hoppers (#911)
    Co-authored-by: DaPigGuy <mcpepig123@gmail.com>

---
+++ server/block/model/hopper.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/world"
+)
+
+// Hopper is a model used by hoppers.
+type Hopper struct{}
+
+// BBox returns a physics.BBox that spans a full block.
+func (h Hopper) BBox(cube.Pos, *world.World) []cube.BBox {
+	bbox := []cube.BBox{full.ExtendTowards(cube.FaceUp, -0.375)}
+	for _, f := range cube.HorizontalFaces() {
+		bbox = append(bbox, full.ExtendTowards(f, -0.875))
+	}
+	return bbox
+}
+
+// FaceSolid only returns true for the top face of the hopper.
+func (Hopper) FaceSolid(_ cube.Pos, face cube.Face, _ *world.World) bool {
+	return face == cube.FaceUp
+}
