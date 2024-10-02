6f85f14d9e3643828118d1179b915b8b8b9444dc
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-10-02 13:54:27 -0400

    use world.BlockSource interface for block models

--- server/block/model/composter.go
+++ server/block/model/composter.go
@@ -1,9 +1,10 @@
 package model
 
 import (
+	"math"
+
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/world"
-	"math"
 )
 
 // Composter is a model used by composter blocks. It is solid on all sides apart from the top, and the height of the
@@ -14,7 +15,7 @@ type Composter struct {
 }
 
 // BBox ...
-func (c Composter) BBox(_ cube.Pos, _ *world.World) []cube.BBox {
+func (c Composter) BBox(_ cube.Pos, _ world.BlockSource) []cube.BBox {
 	compostHeight := math.Abs(math.Min(float64(c.Level), 7)*0.125 - 0.0625)
 	return []cube.BBox{
 		cube.Box(0, 0, 0, 1, 1, 0.125),
@@ -26,6 +27,6 @@ func (c Composter) BBox(_ cube.Pos, _ *world.World) []cube.BBox {
 }
 
 // FaceSolid returns true for all faces other than the top.
-func (Composter) FaceSolid(_ cube.Pos, face cube.Face, _ *world.World) bool {
+func (Composter) FaceSolid(_ cube.Pos, face cube.Face, _ world.BlockSource) bool {
 	return face != cube.FaceUp
 }
