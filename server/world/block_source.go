045fc49de38507f83163e7d9a7c777aaba7b8a70
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-10-15 23:43:02 -0400

    block_source.go: remove SetBlock()

--- server/world/block_source.go
+++ server/world/block_source.go
@@ -2,11 +2,8 @@ package world
 
 import "github.com/df-mc/dragonfly/server/block/cube"
 
-// BlockSource represents a source for blocks. Blocks can be retrieved
-// and set in the block source.
+// BlockSource represents a source for obtaining blocks.
 type BlockSource interface {
 	// Block returns the block at the given position in the block source.
 	Block(cube.Pos) Block
-	// SetBlock sets the block at the given position in the block source.
-	SetBlock(cube.Pos, Block, *SetOpts)
 }
