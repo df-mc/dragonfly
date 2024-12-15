ddfb6201681088b98aa813fbd4ba056806e38fe1
Sandertv <st.ten.veldhuis@gmail.com>
2024-12-15 23:05:50 +0100

    block/sugar_cane.go: Also break on random tick. Resolves #717.

--- server/block/break_info.go
+++ server/block/break_info.go
@@ -5,6 +5,7 @@ import (
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/item/enchantment"
 	"github.com/df-mc/dragonfly/server/world"
+	"github.com/df-mc/dragonfly/server/world/particle"
 	"math"
 	"math/rand"
 	"slices"
@@ -232,5 +233,17 @@ func silkTouchOnlyDrop(it world.Item) func(t item.Tool, enchantments []item.Ench
 			return []item.Stack{item.NewStack(it, 1)}
 		}
 		return nil
+	}
+}
+
+// breakBlock removes a block, shows breaking particles and drops the drops of
+// the block as items.
+func breakBlock(b world.Block, pos cube.Pos, tx *world.Tx) {
+	tx.SetBlock(pos, nil, nil)
+	tx.AddParticle(pos.Vec3Centre(), particle.BlockBreak{Block: b})
+	if breakable, ok := b.(Breakable); ok {
+		for _, drop := range breakable.BreakInfo().Drops(item.ToolNone{}, nil) {
+			dropItem(tx, drop, pos.Vec3Centre())
+		}
 	}
 }
