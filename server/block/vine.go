9c88378ae3cd1f0f50d163b67fb2a61d1dac079e
DaPigGuy <mcpepig123@gmail.com>
2024-12-19 00:47:14 -0800

    block/vine.go: Do not drop with silk touch

--- server/block/vine.go
+++ server/block/vine.go
@@ -35,7 +35,7 @@ func (v Vines) BreakInfo() BreakInfo {
 	return newBreakInfo(0.2, alwaysHarvestable, func(t item.Tool) bool {
 		return t.ToolType() == item.TypeShears || t.ToolType() == item.TypeAxe
 	}, func(t item.Tool, enchantments []item.Enchantment) []item.Stack {
-		if t.ToolType() == item.TypeShears || hasSilkTouch(enchantments) {
+		if t.ToolType() == item.TypeShears {
 			return []item.Stack{item.NewStack(v, 1)}
 		}
 		return nil
