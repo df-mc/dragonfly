b41abece44276f27271cbf0fc90fb4453ad34d08
Sandertv <st.ten.veldhuis@gmail.com>
2024-12-21 01:01:14 +0100

    chat/subscriber.go and chat/chat.go: More documentation.

--- server/player/chat/chat.go
+++ server/player/chat/chat.go
@@ -40,6 +40,10 @@ func (chat *Chat) WriteString(s string) (n int, err error) {
 	return len(s), nil
 }
 
+// Writet writes a Translatable message to a Chat, parameterising the message
+// using the arguments passed. Messages are translated according to the locale
+// of subscribers if they implement Translator. Subscribers that do not
+// implement Translator have the fallback message sent.
 func (chat *Chat) Writet(t Translatable, a ...any) {
 	chat.m.Lock()
 	defer chat.m.Unlock()
--- server/player/chat/subscriber.go
+++ server/player/chat/subscriber.go
@@ -17,7 +17,11 @@ type Subscriber interface {
 	Message(a ...any)
 }
 
+// Translator is a Subscriber that is able to translate messages to their own
+// locale.
 type Translator interface {
+	// Messaget sends a Translatable message to the Translator, using the
+	// arguments passed to fill out any translation parameters.
 	Messaget(t Translatable, a ...any)
 }
 
