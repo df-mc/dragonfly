3dd550fd1ef9270b2eb9166f40ad2aad9a08cdc6
DaPigGuy <mcpepig123@gmail.com>
2024-12-29 05:36:00 -0800

    server/block: Fix Dead Bush placement on Grass/Mud

--- server/block/grass.go
+++ server/block/grass.go
@@ -37,7 +37,7 @@ func init() {
 // SoilFor ...
 func (g Grass) SoilFor(block world.Block) bool {
 	switch block.(type) {
-	case ShortGrass, Fern, DoubleTallGrass, Flower, DoubleFlower, NetherSprouts, PinkPetals, SugarCane:
+	case ShortGrass, Fern, DoubleTallGrass, Flower, DoubleFlower, NetherSprouts, PinkPetals, SugarCane, DeadBush:
 		return true
 	}
 	return false
