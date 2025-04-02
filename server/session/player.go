2a474fdd539cb4416c75a376c1031d8c90a64fe5
Darya Markova <122279000+Dasciam@users.noreply.github.com>
2025-04-02 17:51:16 +0300

    Update player.go

--- server/session/player.go
+++ server/session/player.go
@@ -657,7 +657,6 @@ func (s *Session) uiInventoryFunc(tx *world.Tx, c Controllable) inventory.SlotFu
 		}
 		s.sendInv(s.ui, protocol.WindowIDUI)
 	}
-	
 }
 
 // SendHeldSlot sends the currently held hotbar slot.
