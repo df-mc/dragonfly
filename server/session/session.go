405843c030a778f4cd2c2e49c4f556c070414f6e
Doge <cooldogedev@gmail.com>
2025-04-17 22:00:38 +0300

    server/session.go: Use a channel for non-blocking packet writes (#1054)

--- server/session/session.go
+++ server/session/session.go
@@ -36,6 +36,7 @@ type Session struct {
 	ent      *world.EntityHandle
 	conn     Conn
 	handlers map[uint32]packetHandler
+	packets  chan packet.Packet
 
 	currentScoreboard atomic.Pointer[string]
 	currentLines      atomic.Pointer[[]string]
@@ -152,6 +153,7 @@ func (conf Config) New(conn Conn) *Session {
 		openChunkTransactions:  make([]map[uint64]struct{}, 0, 8),
 		closeBackground:        make(chan struct{}),
 		handlers:               map[uint32]packetHandler{},
+		packets:                make(chan packet.Packet, 256),
 		entityRuntimeIDs:       map[*world.EntityHandle]uint64{},
 		entities:               map[uint64]*world.EntityHandle{},
 		hiddenEntities:         map[uuid.UUID]struct{}{},
@@ -178,6 +180,16 @@ func (conf Config) New(conn Conn) *Session {
 	s.sendRecipes()
 	s.sendArmourTrimData()
 	s.SendSpeed(0.1)
+	go func() {
+		for {
+			select {
+			case <-s.closeBackground:
+				return
+			case pk := <-s.packets:
+				_ = conn.WritePacket(pk)
+			}
+		}
+	}()
 	return s
 }
 
@@ -503,7 +515,10 @@ func (s *Session) writePacket(pk packet.Packet) {
 	if s == Nop {
 		return
 	}
-	_ = s.conn.WritePacket(pk)
+	select {
+	case s.packets <- pk:
+	case <-s.closeBackground:
+	}
 }
 
 // actorIdentifier represents the structure of an actor identifier sent over the network.
