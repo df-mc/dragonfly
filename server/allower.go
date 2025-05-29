6ea250fca7273866b95beecc9bf2e208588413fe
didntpot <didntpot@protonmail.com>
2025-05-29 04:07:28 +0100

    server: Pass the connections protocol to the allower.

--- server/allower.go
+++ server/allower.go
@@ -1,8 +1,10 @@
 package server
 
 import (
-	"github.com/sandertv/gophertunnel/minecraft/protocol/login"
 	"net"
+
+	"github.com/sandertv/gophertunnel/minecraft"
+	"github.com/sandertv/gophertunnel/minecraft/protocol/login"
 )
 
 // Allower may be implemented to specifically allow or disallow players from
@@ -10,18 +12,18 @@ import (
 // call to Server.Allow.
 type Allower interface {
 	// Allow filters what connections are allowed to connect to the Server. The
-	// address, identity data, and client data of the connection are passed. If
-	// Admit returns false, the connection is closed with the string returned as
+	// address, protocol, identity data, and client data of the connection are passed. If
+	// Allow returns false, the connection is closed with the string returned as
 	// the disconnect message. WARNING: Use the client data at your own risk, it
 	// cannot be trusted because it can be freely changed by the player
 	// connecting.
-	Allow(addr net.Addr, d login.IdentityData, c login.ClientData) (string, bool)
+	Allow(addr net.Addr, proto minecraft.Protocol, d login.IdentityData, c login.ClientData) (string, bool)
 }
 
 // allower is the standard Allower implementation. It accepts all connections.
 type allower struct{}
 
 // Allow always returns true.
-func (allower) Allow(net.Addr, login.IdentityData, login.ClientData) (string, bool) {
+func (allower) Allow(net.Addr, minecraft.Protocol, login.IdentityData, login.ClientData) (string, bool) {
 	return "", true
 }
