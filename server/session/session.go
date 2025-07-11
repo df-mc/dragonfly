df-mc/dragonfly

commit dbb8205e8fce9c55076df4cc5918cbacb69c0a7f

session.go: Added support for processing up to 256 packets per transaction (#1092)

server/session/session.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/df-mc/dragonfly/server/player/debug"
 	"io"
 	"log/slog"
 	"net"
@@ -17,6 +16,7 @@ import (
 	"github.com/df-mc/dragonfly/server/item/inventory"
 	"github.com/df-mc/dragonfly/server/item/recipe"
 	"github.com/df-mc/dragonfly/server/player/chat"
+	"github.com/df-mc/dragonfly/server/player/debug"
 	"github.com/df-mc/dragonfly/server/player/form"
 	"github.com/df-mc/dragonfly/server/player/skin"
 	"github.com/df-mc/dragonfly/server/world"
@@ -35,10 +35,11 @@ type Session struct {
 	conf           Config
 	once, connOnce sync.Once
 
-	ent      *world.EntityHandle
-	conn     Conn
-	handlers map[uint32]packetHandler
-	packets  chan packet.Packet
+	ent             *world.EntityHandle
+	conn            Conn
+	handlers        map[uint32]packetHandler
+	outgoingPackets chan packet.Packet
+	incomingPackets chan packet.Packet
 
 	currentScoreboard atomic.Pointer[string]
 	currentLines      atomic.Pointer[[]string]
@@ -92,6 +93,7 @@ type Session struct {
 	debugShapesRemove chan int
 
 	closeBackground chan struct{}
+	closeRead       chan struct{}
 }
 
 // Conn represents a connection that packets are read from and written to by a Session. In addition, it holds some
@@ -159,8 +161,10 @@ func (conf Config) New(conn Conn) *Session {
 	*s = Session{
 		openChunkTransactions:  make([]map[uint64]struct{}, 0, 8),
 		closeBackground:        make(chan struct{}),
+		closeRead:              make(chan struct{}),
 		handlers:               map[uint32]packetHandler{},
-		packets:                make(chan packet.Packet, 256),
+		outgoingPackets:        make(chan packet.Packet, 256),
+		incomingPackets:        make(chan packet.Packet, 256),
 		entityRuntimeIDs:       map[*world.EntityHandle]uint64{},
 		entities:               map[uint64]*world.EntityHandle{},
 		hiddenEntities:         map[uuid.UUID]struct{}{},
@@ -196,8 +200,41 @@ func (conf Config) New(conn Conn) *Session {
 			select {
 			case <-s.closeBackground:
 				return
-			case pk := <-s.packets:
+			case pk := <-s.outgoingPackets:
 				_ = conn.WritePacket(pk)
+			case first := <-s.incomingPackets:
+				var err error
+				s.ent.ExecWorld(func(tx *world.Tx, e world.Entity) {
+					err = s.handlePacket(first, tx, e.(Controllable))
+					if err != nil {
+						return
+					}
+
+					total := len(s.incomingPackets)
+				receive:
+					for i := 0; i < total; i++ {
+						select {
+						case <-s.closeBackground:
+							break receive
+						case pk := <-s.incomingPackets:
+							err = s.handlePacket(pk, tx, e.(Controllable))
+							if err != nil {
+								break receive
+							}
+						default:
+							break receive
+						}
+					}
+				})
+
+				if err != nil {
+					s.conf.Log.Debug("process packet: " + err.Error())
+					select {
+					case <-s.closeRead:
+					default:
+						close(s.closeRead)
+					}
+				}
 			}
 		}
 	}()
@@ -339,12 +376,11 @@ func (s *Session) handlePackets() {
 		if err != nil {
 			return
 		}
-		s.ent.ExecWorld(func(tx *world.Tx, e world.Entity) {
-			err = s.handlePacket(pk, tx, e.(Controllable))
-		})
-		if err != nil {
-			s.conf.Log.Debug("process packet: " + err.Error())
+
+		select {
+		case <-s.closeRead:
 			return
+		case s.incomingPackets <- pk:
 		}
 	}
 }
@@ -526,7 +562,7 @@ func (s *Session) writePacket(pk packet.Packet) {
 		return
 	}
 	select {
-	case s.packets <- pk:
+	case s.outgoingPackets <- pk:
 	case <-s.closeBackground:
 	}
 }

commit 206a416b960523b48793d3a0a80a2c094f5cddac

session.go: Fixed deadlock caused by https://github.com/df-mc/dragonfly/pull/1092

server/session/session.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/df-mc/dragonfly/server/player/debug"
-	"github.com/df-mc/dragonfly/server/player/hud"
 	"io"
 	"log/slog"
 	"net"
@@ -18,7 +16,9 @@ import (
 	"github.com/df-mc/dragonfly/server/item/inventory"
 	"github.com/df-mc/dragonfly/server/item/recipe"
 	"github.com/df-mc/dragonfly/server/player/chat"
+	"github.com/df-mc/dragonfly/server/player/debug"
 	"github.com/df-mc/dragonfly/server/player/form"
+	"github.com/df-mc/dragonfly/server/player/hud"
 	"github.com/df-mc/dragonfly/server/player/skin"
 	"github.com/df-mc/dragonfly/server/world"
 	"github.com/go-gl/mathgl/mgl64"
@@ -207,8 +207,6 @@ func (conf Config) New(conn Conn) *Session {
 			select {
 			case <-s.closeBackground:
 				return
-			case pk := <-s.outgoingPackets:
-				_ = conn.WritePacket(pk)
 			case first := <-s.incomingPackets:
 				var err error
 				s.ent.ExecWorld(func(tx *world.Tx, e world.Entity) {
@@ -241,6 +239,7 @@ func (conf Config) New(conn Conn) *Session {
 					default:
 						close(s.closeRead)
 					}
+					return
 				}
 			}
 		}
@@ -379,6 +378,12 @@ func (s *Session) handlePackets() {
 		})
 	}()
 	for {
+		select {
+		case <-s.closeRead:
+			return
+		default:
+		}
+
 		pk, err := s.conn.ReadPacket()
 		if err != nil {
 			return
@@ -386,13 +391,12 @@ func (s *Session) handlePackets() {
 
 		select {
 		case <-s.closeRead:
-			return
 		case s.incomingPackets <- pk:
 		}
 	}
 }
 
-// background performs background tasks of the Session. This includes chunk sending and automatic command updating.
+// background performs background tasks of the Session. This includes chunk sending, automatic command updating, and packet writing.
 // background returns when the Session's connection is closed using CloseConnection.
 func (s *Session) background() {
 	var (
@@ -430,6 +434,8 @@ func (s *Session) background() {
 				}
 				s.sendChunks(tx, c)
 			})
+		case pk := <-s.outgoingPackets:
+			_ = s.conn.WritePacket(pk)
 		case <-s.closeBackground:
 			return
 		}