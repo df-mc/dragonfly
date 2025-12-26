7f56f38b074f77b71ca15f15c7583b52e4ba7f73
didntpot <didntpot@protonmail.com>
2025-12-26 10:28:08 +0000

    block/double_tall_grass.go: Fixed drop rate, implemented fortune.

--- server/block/double_tall_grass.go
+++ server/block/double_tall_grass.go
@@ -1,11 +1,12 @@
 package block
 
 import (
+	"math/rand/v2"
+
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/world"
 	"github.com/go-gl/mathgl/mgl64"
-	"math/rand/v2"
 )
 
 // DoubleTallGrass is a two-block high variety of grass.
@@ -61,8 +62,12 @@ func (d DoubleTallGrass) BreakInfo() BreakInfo {
 		if t.ToolType() == item.TypeShears || hasSilkTouch(enchantments) {
 			return []item.Stack{item.NewStack(d, 1)}
 		}
-		if rand.Float32() > 0.57 {
-			return []item.Stack{item.NewStack(WheatSeeds{}, 1)}
+		if rand.Float32() < 0.125 {
+			count := 1
+			if fortune := fortuneLevel(enchantments); fortune > 0 {
+				count += rand.IntN(fortune*2 + 1)
+			}
+			return []item.Stack{item.NewStack(WheatSeeds{}, count)}
 		}
 		return nil
 	})
