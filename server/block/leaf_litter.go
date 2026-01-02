fbe118dcd814a9c2a728515a7514361b8f6754f3
Niceninjapro <niceninjapro@gmail.com>
2026-01-02 15:39:08 +0000

    Implement LeafLitter block with methods
    Added LeafLitter block with break and encode methods.

--- server/block/leaf_litter.go
+++ server/block/leaf_litter.go
@@ -1,1 +1,21 @@
+package block
 
+// LeafLitter is a bioluminescent block found abundantly in the deep dark
+type LeafLitter struct {
+	solid
+}
+
+// BreakInfo ...
+func (l LeafLitter) BreakInfo() BreakInfo {
+	return newBreakInfo(0.2, alwaysHarvestable, hoeEffective, silkTouchOnlyDrop(l)).withXPDropRange(1, 1)
+}
+
+// EncodeItem ...
+func (l LeafLitter) EncodeItem() (name string, meta int16) {
+	return "minecraft:leaf_litter", 0
+}
+
+// EncodeBlock ...
+func (l LeafLitter) EncodeBlock() (name string, properties map[string]any) {
+	return "minecraft:leaf_litter", nil
+}
