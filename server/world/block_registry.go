174766521e1243722990d9ad9edbf7c3a8c59c6a
HashimTheArab <hashimssoftware@gmail.com>
2026-01-14 21:52:16 +0300

    refactor: remove unused code

--- server/world/block_registry.go
+++ server/world/block_registry.go
@@ -7,11 +7,9 @@ import (
 	"math/bits"
 	"slices"
 	"sort"
-	"strings"
 
 	"github.com/df-mc/dragonfly/server/internal/intintmap"
 	"github.com/df-mc/dragonfly/server/world/chunk"
-	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/segmentio/fasthash/fnv1"
 )
 
@@ -489,105 +487,4 @@ func (br *BlockRegistryImpl) Air() Block {
 		panic("BlockRegistry.Air: air runtime ID out of range")
 	}
 	return br.blocks[br.airRID]
-}
-
-var traitLookup = map[string][]any{
-	"minecraft:facing_direction": {
-		"north", "east", "south", "west", "down", "up",
-	},
-	"minecraft:cardinal_direction": {
-		"north", "east", "south", "west",
-	},
-	"minecraft:vertical_half": {
-		"top", "bottom",
-	},
-	"minecraft:block_face": {
-		"north", "east", "south", "west", "down", "up",
-	},
-}
-
-func AddCustomBlocks(reg BlockRegistry, entries []protocol.BlockEntry) error {
-	for _, entry := range entries {
-		ns, _ := splitNamespace(entry.Name)
-		if ns == "minecraft" {
-			continue
-		}
-
-		var propertyNames []string
-		var propertyValues [][]any
-
-		props, ok := entry.Properties["properties"].([]any)
-		if ok {
-			for _, v := range props {
-				v := v.(map[string]any)
-				name := v["name"].(string)
-				enum := v["enum"].([]any)
-				propertyNames = append(propertyNames, name)
-				propertyValues = append(propertyValues, enum)
-			}
-		}
-
-		traits, ok := entry.Properties["traits"].([]any)
-		if ok {
-			for _, trait := range traits {
-				trait := trait.(map[string]any)
-				enabled_states := trait["enabled_states"].(map[string]any)
-				for k, enabled := range enabled_states {
-					if !strings.ContainsRune(k, ':') {
-						k = "minecraft:" + k
-					}
-					enabled := enabled.(uint8)
-					if enabled == 0 {
-						continue
-					}
-					v, ok := traitLookup[k]
-					if !ok {
-						return fmt.Errorf("unresolved trait %s", k)
-					}
-
-					propertyNames = append(propertyNames, k)
-					propertyValues = append(propertyValues, v)
-				}
-			}
-		}
-
-		permutations := cartesianProduct(propertyValues)
-
-		for _, values := range permutations {
-			m := make(map[string]any)
-			for i, value := range values {
-				name := propertyNames[i]
-				m[name] = value
-			}
-			reg.RegisterBlockState(BlockState{
-				Name:       entry.Name,
-				Properties: m,
-			})
-		}
-	}
-
-	return nil
-}
-
-func cartesianProduct(inputs [][]any) [][]any {
-	if len(inputs) == 0 {
-		return [][]any{{}}
-	}
-	res := make([][]any, 0)
-	current := make([]any, len(inputs))
-	var recur func(int)
-	recur = func(idx int) {
-		if idx == len(inputs) {
-			comb := make([]any, len(current))
-			copy(comb, current)
-			res = append(res, comb)
-			return
-		}
-		for _, v := range inputs[idx] {
-			current[idx] = v
-			recur(idx + 1)
-		}
-	}
-	recur(0)
-	return res
 }
