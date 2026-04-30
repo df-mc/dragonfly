e44d44ff4827dc792fd6475019f83bfce8526caf
hydzilla <kingofsaber@proton.me>
2026-04-30 07:31:51 +0100

    server/item: Implement Honey Bottle

---
+++ server/item/honey_bottle.go
@@ -0,0 +1,39 @@
+package item
+
+import (
+	"time"
+
+	"github.com/df-mc/dragonfly/server/entity/effect"
+	"github.com/df-mc/dragonfly/server/world"
+)
+
+// HoneyBottle is a food item obtained from full beehives or beenests using a glass bottle. Consuming it
+// restores hunger, removes any active poison and returns an empty glass bottle.
+type HoneyBottle struct{}
+
+// MaxCount ...
+func (HoneyBottle) MaxCount() int {
+	return 16
+}
+
+// AlwaysConsumable ...
+func (HoneyBottle) AlwaysConsumable() bool {
+	return true
+}
+
+// ConsumeDuration is longer than DefaultConsumeDuration to match the slower drinking animation.
+func (HoneyBottle) ConsumeDuration() time.Duration {
+	return DefaultConsumeDuration * 5 / 4
+}
+
+// Consume ...
+func (HoneyBottle) Consume(_ *world.Tx, c Consumer) Stack {
+	c.Saturate(6, 1.2)
+	c.RemoveEffect(effect.Poison)
+	return NewStack(GlassBottle{}, 1)
+}
+
+// EncodeItem ...
+func (HoneyBottle) EncodeItem() (name string, meta int16) {
+	return "minecraft:honey_bottle", 0
+}
