45ed62d57adcca4906a9315555a558a50240efe1
schphe <git@emin.sh>
2026-08-11 18:25:53 -0500

    Remove the comment added by this change

--- server/conf.go
+++ server/conf.go
@@ -195,7 +195,6 @@ func (conf Config) New() *Server {
 		l, err := lf(conf)
 		if err != nil {
 			conf.Log.Error("create listener: " + err.Error())
-			// The listener is nil when it could not be created and must not be kept.
 			continue
 		}
 		srv.listeners = append(srv.listeners, l)
