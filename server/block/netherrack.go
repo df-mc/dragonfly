7d624fb6a466bf277a675ec462cd80e7efb85990
MEMOxiiii <189345814+MEMOxiiii@users.noreply.github.com>
2026-08-16 14:21:15 +0300

    add crimson and warped nylium

--- server/block/netherrack.go
+++ server/block/netherrack.go
@@ -1,6 +1,10 @@
 package block
 
 import (
+	"math/rand/v2"
+	"slices"
+
+	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/world"
 )
@@ -15,6 +19,26 @@ type Netherrack struct {
 func (n Netherrack) SoilFor(block world.Block) bool {
 	flower, ok := block.(Flower)
 	return ok && flower.Type == WitherRose()
+}
+
+// BoneMeal ...
+func (n Netherrack) BoneMeal(pos cube.Pos, tx *world.Tx) item.BoneMealResult {
+	var types []Nylium
+	for x := -1; x <= 1; x++ {
+		for z := -1; z <= 1; z++ {
+			if x == 0 && z == 0 {
+				continue
+			}
+			if nylium, ok := tx.Block(pos.Add(cube.Pos{x, 0, z})).(Nylium); ok && !slices.Contains(types, nylium) {
+				types = append(types, nylium)
+			}
+		}
+	}
+	if len(types) == 0 {
+		return item.BoneMealResultNone
+	}
+	tx.SetBlock(pos, types[rand.IntN(len(types))], nil)
+	return item.BoneMealResultSmall
 }
 
 // BreakInfo ...
