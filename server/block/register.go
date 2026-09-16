3073c610c3af7f40ded50e2b62bfa6f18cac899b
MEMOxiiii <189345814+MEMOxiiii@users.noreply.github.com>
2026-09-16 22:26:36 +0300

    server/block: Implement ice (#1438)

--- server/block/register.go
+++ server/block/register.go
@@ -67,6 +67,7 @@ func init() {
 	world.RegisterBlock(Grass{})
 	world.RegisterBlock(Gravel{})
 	world.RegisterBlock(Honeycomb{})
+	world.RegisterBlock(Ice{})
 	world.RegisterBlock(InfestedStone{})
 	world.RegisterBlock(InfestedCobblestone{})
 	for _, b := range allInfestedStoneBricks() {
@@ -341,6 +342,7 @@ func init() {
 	world.RegisterItem(HayBale{})
 	world.RegisterItem(Honeycomb{})
 	world.RegisterItem(Hopper{})
+	world.RegisterItem(Ice{})
 	world.RegisterItem(InfestedStone{})
 	world.RegisterItem(InfestedCobblestone{})
 	for _, t := range StoneBricksTypes() {
